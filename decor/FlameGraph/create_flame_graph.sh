5ba8ca65b25d73d127e79151b80296663dcb5962
tslilyai <tslilyai@mit.edu>
2020-10-13 17:08:41 -0400

    add flamegraphs for shim and parse

--- decor/FlameGraph/create_flame_graph.sh
+++ decor/FlameGraph/create_flame_graph.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 name=$1
+testname=$2
 
-sudo perf record -F 99 -g ../target/release/lobsters --ncomments=1000 --nqueries=1000
+sudo perf record -F 99 -g ../target/release/lobsters --test=$testname --ncomments=1000 --nqueries=1000
 sudo perf script | ./stackcollapse-perf.pl > out.perf-folded
 sudo ./flamegraph.pl out.perf-folded > $name.svg
 
