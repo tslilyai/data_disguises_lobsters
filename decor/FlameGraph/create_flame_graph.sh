a5e96ede9120169c6a1d62882e8bf6293ca303b6
Lily Tsai <tslilyai@mit.edu>
2020-10-14 16:42:26 -0700

    keep frame pointers

--- decor/FlameGraph/create_flame_graph.sh
+++ decor/FlameGraph/create_flame_graph.sh
@@ -4,11 +4,12 @@ set +x
 
 tests=( "no_shim" "shim_only" "shim_parse" "decor" )
 
-cargo build
+cargo clean
+RUSTFLAGS='-C force-frame-pointers=y' cargo build --release
 
 for test in "${tests[@]}"
 do
-    sudo perf record -g -a -o $test.perf.data ../target/debug/lobsters --test=$test --nusers=10 --nstories=100 --ncomments=1000 --nthreads=1 --nqueries=3000 #2> /dev/null
+    sudo perf record -g -a -o $test.perf.data ../target/release/lobsters --test=$test --nusers=10 --nstories=100 --ncomments=1000 --nthreads=1 --nqueries=3000 #2> /dev/null
     sudo perf script | ./stackcollapse-perf.pl > out.perf-folded
     sudo ./flamegraph.pl out.perf-folded > jerry-$test.svg
 done
