1eeb0c84fe8782259a066ad8333c8bfa14fe103c
tslilyai <tslilyai@mit.edu>
2020-10-23 21:07:14 -0400

    update flame graphs with caching

--- decor/create_flame_graph.sh
+++ decor/create_flame_graph.sh
@@ -10,7 +10,7 @@ cargo build --release
 for test in "${tests[@]}"
 do
 	echo $test
-	cargo flamegraph -o $test-select.svg --bin lobsters -- --test=$test --nusers=10 --nstories=100 --ncomments=1000 --nthreads=1 --nqueries=3000 --testop=select
-	cargo flamegraph -o $test-insert.svg --bin lobsters -- --test=$test --nusers=10 --nstories=100 --ncomments=1000 --nthreads=1 --nqueries=3000 --testop=insert
-	cargo flamegraph -o $test-update.svg --bin lobsters -- --test=$test --nusers=10 --nstories=100 --ncomments=1000 --nthreads=1 --nqueries=3000 --testop=update
+	cargo flamegraph -o flamegraphs/$test-select.svg --bin lobsters -- --test=$test --nusers=10 --nstories=100 --ncomments=1000 --nthreads=1 --nqueries=3000 --testop=select
+	cargo flamegraph -o flamegraphs/$test-insert.svg --bin lobsters -- --test=$test --nusers=10 --nstories=100 --ncomments=1000 --nthreads=1 --nqueries=3000 --testop=insert
+	cargo flamegraph -o flamegraphs/$test-update.svg --bin lobsters -- --test=$test --nusers=10 --nstories=100 --ncomments=1000 --nthreads=1 --nqueries=3000 --testop=update
 done
