6e1a4b731ca4a1a26ccb2fd0b75d0783d1a44f83
tslilyai <tslilyai@mit.edu>
2020-11-16 08:26:19 -0500

    add flamegraph for inmem decor

--- decor/applications/lobsters/microbenchmarks/main.rs
+++ decor/applications/lobsters/microbenchmarks/main.rs
@@ -266,16 +266,15 @@ fn main() {
     for i in 0..nqueries {
         let user = users[((i % nusers) as usize)];
         let story= stories[((i+1)%nstories) as usize];
-        let comments= comments[((i+2)%ncomments) as usize];
         match rng.gen_range(0, 10) {
-            0..=5 => queriers::frontpage::query_frontpage(&mut db, Some(user)).unwrap(),
+            /*0..=5 => queriers::frontpage::query_frontpage(&mut db, Some(user)).unwrap(),
             6 => {
                 queriers::post_story::post_story(&mut db, Some(user), total_stories + 1, "Dummy title".to_string()).unwrap();
                 total_stories += 1;
             }
             7 => queriers::vote::vote_on_story(&mut db, Some(user), story, true).unwrap(),
-            8 => queriers::user::get_profile(&mut db, user).unwrap(),
-            9 => {
+            8 => queriers::user::get_profile(&mut db, user).unwrap(),*/
+            0..=9 => {
                 queriers::comment::post_comment(&mut db, Some(user), total_comments + 1, story, None).unwrap();
                 total_comments += 1;
             }
--- decor/benchmarks/create_flame_graph.sh
+++ decor/benchmarks/create_flame_graph.sh
@@ -2,7 +2,8 @@
 
 set +x
 
-tests=( "no_shim" "shim_only" "shim_parse" "decor" )
+#tests=( "no_shim" "shim_only" "shim_parse" "decor" )
+tests=( "decor" ) #"shim_only" "shim_parse" "decor" )
 
 cargo clean
 cargo build --release
@@ -10,7 +11,5 @@ cargo build --release
 for test in "${tests[@]}"
 do
 	echo $test
-	cargo flamegraph -o flamegraphs/$test-select.svg --bin lobsters-microbenchmarks -- --test=$test --nusers=10 --nstories=100 --ncomments=1000 --nthreads=1 --nqueries=3000 --testop=select
-	cargo flamegraph -o flamegraphs/$test-insert.svg --bin lobsters-microbenchmarks -- --test=$test --nusers=10 --nstories=100 --ncomments=1000 --nthreads=1 --nqueries=3000 --testop=insert
-	cargo flamegraph -o flamegraphs/$test-update.svg --bin lobsters-microbenchmarks -- --test=$test --nusers=10 --nstories=100 --ncomments=1000 --nthreads=1 --nqueries=3000 --testop=update
+	cargo flamegraph -o flamegraphs/$test.svg --bin lobsters-microbenchmarks -- --test=$test --nusers=10 --nstories=100 --ncomments=1000 --nthreads=1 --nqueries=300 
 done
