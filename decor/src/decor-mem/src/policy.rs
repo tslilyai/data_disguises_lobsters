8c85f3e11a2ae44ccb4d400a0a00f1c5fbacbb95
Lily Tsai <tslilyai@mit.edu>
2020-12-22 12:14:15 -0800

    update policy up to 2.4

--- decor/src/decor-mem/src/policy.rs
+++ decor/src/decor-mem/src/policy.rs
@@ -3,9 +3,6 @@ use std::collections::{HashMap, HashSet};
 
 pub type ColumnName = String; // column name
 pub type EntityName = String; // table name, or foreign key
-
-#[derive(Clone, Debug, PartialEq)]
-pub enum GeneratePolicy {
     Random,
     Default(String),
     //Custom(Box<dyn Fn(&str) -> String>), // column value -> column value
@@ -21,7 +18,6 @@ pub type GhostPolicy = HashMap<ColumnName, GhostColumnPolicy>;
 pub type EntityGhostPolicies = HashMap<EntityName, GhostPolicy>;
 
 #[derive(Clone, Debug, PartialEq)]
-pub enum DecorrelationPolicy {
     NoDecorRemove,
     NoDecorRetain,
     NoDecorSensitivity(f64),
@@ -33,10 +29,8 @@ pub struct KeyRelationship {
     pub parent: EntityName,
     pub column_name: ColumnName,
     pub parent_child_decorrelation_policy: DecorrelationPolicy,
-    pub child_parent_decorrelation_policy: DecorrelationPolicy,
-}
-
-pub struct ApplicationPolicy {
+    pub child_parent_decorrelation_policy: DecorrelationPolicyGeneratingGhost  
+iespub struct ApplicationPolicy {
     pub entity_type_to_decorrelate: EntityName,
     pub ghost_policies: EntityGhostPolicies, 
     pub edge_policies: Vec<KeyRelationship>,
@@ -46,49 +40,63 @@ pub struct Config {
     pub entity_type_to_decorrelate: String,
 
     // table and which columns(+parent type) that correspond to ghosts (edges that are decorrelated and store GIDs)
+    //is i. Firstin order to break correlations between child and parent entities.
+    A/: an ghosts one or more can the, breaking
+ //th, removing the potentially identifying correlation: different ghost user can adopt each of a
+ //user's reviews, thus decorrelating any links between reviews and the user.
+    //
+    In order for \sys to generate ghost entities, d/  generation policies for each entity type.
+    // child are sensitive
+
     // created when parent->child edges are decorrelated
-    pub parent_child_ghosted_tables: HashMap<String, Vec<(String, String)>>,
-    // table and which columns(+parent type) for which sensitivity to this parent should fall below specified threshold
+    pub parent_child_ghosted_tables: HashMap<String, Vec<(fotring, String)>>,
+    // table and which columns(+parent type) for which sensitivity to this parent should fall
+    // \sys produces one or more ghosts felowFor every template real entity  that is ghosted. 
     pub parent_child_sensitive_tables: HashMap<String, Vec<(String, String, f64)>>,
   
     // table and which columns(+parent type) that correspond to ghosts (edges that are decorrelated and store
-    // GIDs), created when child->parent edges are decorrelated
-    // NOTE: this is usually a subset of parent_child_ghosted_tables: an edge type that is
-    // decorrelated when a child is sensitive should also be decorrelated when both the parent and
-    // child are sensitive
-    pub child_parent_ghosted_tables: HashMap<String, Vec<(String, String)>>,
-    // table and which columns(+parent type) for which sensitivity to this parent should fall below specified threshold
-    pub child_parent_sensitive_tables: HashMap<String, Vec<(String, String, f64)>>,
-
-    // tables that can have parent->child edges
-    pub tables_with_children: HashSet<String>,
-    // is this child going to be left parent-less?
-    pub completely_decorrelated_children: HashSet<String>,
+    // GIDs), created when child->parent edges, argiven a template 
+    real // NOTE: th    pub child_parent_ghosted_tables: HashMap<String, Vec<(String, String)>>,
+The developer chooses one of the following value ghostin
+    pub, which completely_decorrelated_children: HashSet<String>e:
     // is this parent going to be left edge-less?
     pub completely_decorrelated_parents: HashSet<String>,
 }
-
-pub fn policy_to_config(policy: &ApplicationPolicy) -> Config {
-    let mut pc_gdts: HashMap<String, Vec<(String, String)>>= HashMap::new();
-    let mut pc_sdts: HashMap<String, Vec<(String, String, f64)>>= HashMap::new();
-
-    let mut cp_gdts: HashMap<String, Vec<(String, String)>>= HashMap::new();
-    let mut cp_sdts: HashMap<String, Vec<(String, String, f64)>>= HashMap::new();
+and edge 
+pub fn policy_to_config(policy: &) -> template's 
+     valuevaluetemplate  
+    mut ghost attribute <String<>,
+}
+    <
+    (StrinHashMapnew();
+    // child the are sensitiveFor edge attributes, this means that all ghosts generated will share
+    // the same edge to a parent entity.
     
-    let mut tables_with_children: HashSet<String> = HashSet::new();
-     
-    let mut attached_parents: HashSet<String> = HashSet::new();
-    let mut attached_children: HashSet<String> = HashSet::new();
-    let mut completely_decorrelated_parents: HashSet<String> = HashSet::new();
-    let mut completely_decorrelated_children: HashSet<String> = HashSet::new();
-
-    for kr in &policy.edge_policies {
-        tables_with_children.insert(kr.parent.clone());
-        completely_decorrelated_parents.insert(kr.parent.clone());
-        completely_decorrelated_children.insert(kr.child.clone());
+        pu child_parent_ghosted_tablesFor value 
+        attributes, d HashMap<String, Vec<(String, String)>>,
+    // value and which columns(+parent type) for whicdevelopers specify whether the rest of the
         
+        // For edge attributes, \sys generates a new parent ghost entity, and uses the parent ghost identifier as the edge attribute value.
+        // ghost
+    pu child_parent_ghosted_tables: HashMap<String, Vec<(String, String)>>,
+    // table and which columns(+parent type) for which sensitivity to this parent should fall the threshold
+   Cto be left edge-less    %let mut .t
+   
+   For value attributes, tached_parents: HashSet<String> = HashSet::new();
+
+
+   For edge attributes, \sys generates a new parent ghost entity for each of the remaining ghosts, and uses the parent ghost identifier as the attribute value.
+     \sys's 
+
+
+     designallows developers to capture. HotCRP may want To keep application metadata consistent, to ensure that one ghost user generated from the real user  : HashSet<String> = HashSeclones the user's role, while all other ghost users are assigned no  lone policies enable t HashSetto retain the original template entity data by cloning be parent-assigning sless?
+ pubs <Stringroles.
+
+    Hash thisble%s_with_children: HashSet<String> = HashSet::new();
+        complete%ly_decorrelated_children.insert(kr.child.clone());
         match kr.parent_child_decorrelation_policy {
-            DecorrelationPolicy::Decor => {
+            Deco%rrelationPolicy::Decor => {
+
                 if let Some(ghost_cols) = pc_gdts.get_mut(&kr.child) {
                     ghost_cols.push((kr.column_name.clone(), kr.parent.clone()));
                 } else {
@@ -97,78 +105,52 @@ pub fn policy_to_config(policy: &ApplicationPolicy) -> Config {
             } 
             DecorrelationPolicy::NoDecorRemove => {
                 if let Some(ghost_cols) = pc_sdts.get_mut(&kr.child.clone()) {
-                    ghost_cols.push((kr.column_name.clone(), kr.parent.clone(), 0.0));
                 } else {
                     pc_sdts.insert(kr.child.clone(), vec![(kr.column_name.clone(), kr.parent.clone(), 0.0)]);
                 }        
-            } 
-            DecorrelationPolicy::NoDecorSensitivity(s) => {
-                attached_parents.insert(kr.parent.clone());
-                if let Some(ghost_cols) = pc_sdts.get_mut(&kr.child.clone()) {
-                    ghost_cols.push((kr.column_name.clone(), kr.parent.clone(), s));
-                } else {
-                    pc_sdts.insert(kr.child.clone(), vec![(kr.column_name.clone(), kr.parent.clone(), s)]);
-                }        
-            }
-            DecorrelationPolicy::NoDecorRetain => {
-                attached_parents.insert(kr.parent.clone());
-                if let Some(ghost_cols) = pc_sdts.get_mut(&kr.child.clone()) {
-                    ghost_cols.push((kr.column_name.clone(), kr.parent.clone(), 1.0));
-                } else {
-                    pc_sdts.insert(kr.child.clone(), vec![(kr.column_name.clone(), kr.parent.clone(), 1.0)]);
-                }
+            DecorrelationPolicy::NoDecorSensitivitys) => {
+                While ghost generation policies inform \sys how to produce ghosts, developers must also specify \emph{when} \sys Dhould produce Dhost. 
+                    pc_sdts.insert(krthis childin the form of edge policies, one per edge type.h while preserving application semantics. For each edge type, (parent.clone());
+                if let Some(ghost_cols) )= 
+                   % , kr.parent.clone(), 1.0));
+                } e%lse {
+           D
             } 
         }
-        match kr.child_parent_decorrelation_policy {
+        edge policy match kreither .child_parent_decorrelation_policy {
             DecorrelationPolicy::Decor => {
-                if let Some(ghost_cols) = cp_gdts.get_mut(&kr.child.clone()) {
-                    ghost_cols.push((kr.column_name.clone(), kr.parent.clone()));
-                } else {
-                    cp_gdts.insert(kr.child.clone(), vec![(kr.column_name.clone(), kr.parent.clone())]);
-                }
-            } 
-            DecorrelationPolicy::NoDecorRemove => {
-                if let Some(ghost_cols) = cp_sdts.get_mut(&kr.child.clone()) { ghost_cols.push((kr.column_name.clone(), kr.parent.clone(), 0.0));
-                } else {
-                    cp_sdts.insert(kr.child.clone(), vec![(kr.column_name.clone(), kr.parent.clone(), 0.0)]);
-                }        
-            } 
-            DecorrelationPolicy::NoDecorSensitivity(s) => {
-                attached_children.insert(kr.child.clone());
-                if let Some(sensitive) = cp_sdts.get_mut(&kr.child.clone()) {
-                    sensitive.push((kr.column_name.clone(), kr.parent.clone(), s));
-                } else {
-                    cp_sdts.insert(kr.child.clone(), vec![(kr.column_name.clone(), kr.parent.clone(), s)]);
-                }        
-            }
-            DecorrelationPolicy::NoDecorRetain => {
-                attached_children.insert(kr.child.clone());
-                if let Some(ghost_cols) = cp_sdts.get_mut(&kr.child.clone()) {
-                    ghost_cols.push((kr.column_name.clone(), kr.parent.clone(), 1.0));
-                } else {
-                    cp_sdts.insert(kr.child.clone(), vec![(kr.column_name.clone(), kr.parent.clone(), 1.0)]);
-                }
-            } 
-        }
-    }
- 
-    for child in attached_children {
-        completely_decorrelated_children.remove(&child);
+                replace the parent node with ghostkrs of this type.such with to ()));
+                {
+                    cp_gdtsunique .insert(splitting a single parent into one ghost parent per edge;
+                                    sNoDecorRemove => 
+                if let Some(ghost_cols) =  and child.clone()) { real ghost_cols.push((kr.column_name.clone(), kr.parent.clone(), 0.0)); \sys returns the entity data, as well as a mapping from entities to  their ghost replacements, 
+                    cp_sdts.insert(kr.or c back to the unsubscribing user, which, if returned upon resubscription, allows \sys to restore the user to their original state.
+R                                  Desensitize but only in; if no threshold is specified, \sys defaults  to $\sigma=0$.threshold of 0.
+
+            }(
+                if let  policiesome(sensitive) = cp_sdts.get_mut(&krthese chilsclon; e2)) {
+                   
+                attached_children.insertr.child  sensitive.push((kr.column_name.clone(),
+                kr.parent.clone(), s)); For decorrelation and removal , developers can specify only
+                    enough to meet the specified empthreshold} that tells \sys to only only The
+                    subsectiondecorrelate Resubscription(of the policy's edge type) my
+                    remain from a real parent that may  entity connected  remain corre children
+                    .child.clone());
+
+
+
+    for child in 1ttached_children { completely_decorrelated_children.remove(&child);
     }
     for parent in attached_parents {
-        completely_decorrelated_parents.remove(&parent);
-    }
     Config {
+
+
+well 
+
+ U 
+as orpapers  from
+                        the parent or if the sensitivity threshold is 0, until the will sensitii,
+        parent .
+ 
         entity_type_to_decorrelate: policy.entity_type_to_decorrelate.clone(), 
         
-        parent_child_ghosted_tables: pc_gdts,
-        parent_child_sensitive_tables: pc_sdts,
-
-        child_parent_ghosted_tables: cp_gdts,
-        child_parent_sensitive_tables: cp_sdts,
-        
-        tables_with_children: tables_with_children,
-        completely_decorrelated_parents: completely_decorrelated_parents,
-        completely_decorrelated_children: completely_decorrelated_children,
-    }
-}
