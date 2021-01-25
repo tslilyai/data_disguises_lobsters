a6ef5d3128532b872993a234955734994ef557e6
Lily Tsai <tslilyai@mit.edu>
2021-01-24 21:12:35 -0800

    testing lobsters perf

--- decor/benchmarks/run_tests.sh
+++ decor/benchmarks/run_tests.sh
@@ -7,12 +7,12 @@ trials=1
 #tests=( "no_shim" "decor" "shim_only" "shim_parse" )
 #tests=( "decor" )
 
-cargo build --release 
+cargo build --release
 
 for trial in `seq $trials`
 do
 	perflock ../target/release/lobsters \
-		--scale=0.5 --nqueries=10000 --prop_unsub=0.0 --prime
+		--scale=0.01 --nqueries=10000 --prop_unsub=0.0 --prime
 done
 
 python3 plot.py
