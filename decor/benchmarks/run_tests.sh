6bb048400351af680d058929553652c016ee2540
tslilyai <tslilyai@mit.edu>
2021-01-25 00:18:02 -0500

    debug

--- decor/benchmarks/run_tests.sh
+++ decor/benchmarks/run_tests.sh
@@ -8,11 +8,12 @@ trials=1
 #tests=( "decor" )
 
 cargo build --release 
+cargo build 
 
 for trial in `seq $trials`
 do
-	perflock ../target/release/lobsters \
-		--scale=0.5 --nqueries=10000 --prop_unsub=0.0 --prime
+	perflock ../target/debug/lobsters \
+		--scale=0.01 --nqueries=10000 --prop_unsub=0.0 --prime
 done
 
 python3 plot.py
