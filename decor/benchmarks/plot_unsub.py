c9cdeb660d6480519011f15d36c8deb79aba4988
Lily Tsai <tslilyai@mit.edu>
2021-02-02 14:53:41 -0800

    some nits in related, update graph

--- decor/benchmarks/plot_unsub.py
+++ decor/benchmarks/plot_unsub.py
@@ -2,10 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import csv
 
-plt.style.use('seaborn-deep')
-num_qtypes = 6
-
-tests = ["decor_unsub"]
 ybounds = [1000000, 1000000, 1000000, 10000, 6000, 6000]
 
 # collect all results, compute maximum latency over all tests + all query  types
@@ -13,20 +9,32 @@ nobjs = []
 disg = []
 rev = []
 
-for test in tests:
-    with open('{}.out'.format(test),'r') as csvfile:
-        rows = csvfile.readlines()
-        for (i, row) in enumerate(rows):
-            p = row.split(',')
-            nobjs.append(int(p[1]))
-            disg.append(int(p[2])/1000000)
-            rev.append(int(p[3])/1000000)
+with open('{}.out'.format("decor_unsub"),'r') as csvfile:
+    rows = csvfile.readlines()
+    for row in rows:
+        p = row.split(',')
+        nobjs.append(int(p[1]))
 
-    plt.figure(figsize=(5,3.5))
-    plt.plot(nobjs, disg, color='red', linestyle='--', marker="o", label="Disguise")
-    plt.plot(nobjs, rev, color='blue', marker="x", label="Reveal")
-    plt.xlabel("#Objects Associated with User")
-    plt.ylabel("Latency (s)")
-    plt.legend()
-    plt.tight_layout()
-    plt.savefig('{}.pdf'.format(test), dpi=300)
+with open('{}.csv'.format("decor_unsub"),'r') as csvfile:
+    rows = csvfile.readlines()
+    disrow = rows[0]
+    revrow = rows[1]
+
+    pairs = disrow.split(';')[:-1]
+    for p in pairs:
+        p = p.split(',')
+        disg.append(int(p[0]))
+
+    pairs = revrow.split(';')[:-1]
+    for p in pairs:
+        p = p.split(',')
+        rev.append(int(p[0]))
+
+plt.figure(figsize=(5,3))
+plt.plot(nobjs, disg, color='red', linestyle='--', marker="o", label="Disguise")
+plt.plot(nobjs, rev, color='blue', marker="x", label="Reveal")
+plt.xlabel("#Objects Created by User")
+plt.ylabel("#Objects Modified")
+plt.legend()
+plt.tight_layout()
+plt.savefig('{}.pdf'.format("perf"), dpi=300)
