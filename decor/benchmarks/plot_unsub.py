f0744e785d5b5425f27b20e2d8e1bf099f91eab7
Lily Tsai <tslilyai@mit.edu>
2021-02-01 16:54:49 -0800

    use seconds

--- decor/benchmarks/plot_unsub.py
+++ decor/benchmarks/plot_unsub.py
@@ -19,14 +19,14 @@ for test in tests:
         for (i, row) in enumerate(rows):
             p = row.split(',')
             nobjs.append(int(p[1]))
-            disg.append(int(p[2])/1000)
-            rev.append(int(p[3])/1000)
+            disg.append(int(p[2])/1000000)
+            rev.append(int(p[3])/1000000)
 
     plt.figure(figsize=(5,3.5))
     plt.plot(nobjs, disg, color='red', linestyle='--', marker="o", label="Disguise")
     plt.plot(nobjs, rev, color='blue', marker="x", label="Reveal")
     plt.xlabel("#Objects Associated with User")
-    plt.ylabel("Latency (ms)")
+    plt.ylabel("Latency (s)")
     plt.legend()
     plt.tight_layout()
     plt.savefig('{}.png'.format(test), dpi=300)
