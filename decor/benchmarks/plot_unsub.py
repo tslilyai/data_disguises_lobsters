15c2e3c1d00af638993e29a3d4e453da0eb54c23
tslilyai <tslilyai@mit.edu>
2021-02-01 19:20:12 -0500

    update run_tests

---
+++ decor/benchmarks/plot_unsub.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+plt.style.use('seaborn-deep')
+num_qtypes = 6
+
+tests = ["decor_unsub"]
+names = ["Disguise", "Reveal"]
+ybounds = [1000000, 1000000, 1000000, 10000, 6000, 6000]
+
+# collect all results, compute maximum latency over all tests + all query  types
+nobjs = []
+disg = []
+rev = []
+
+for test in tests:
+    with open('{}.out'.format(test),'r') as csvfile:
+        rows = csvfile.readlines()
+        for (i, row) in enumerate(rows):
+            p = row.split(',')
+            nobjs.append(int(p[1]))
+            disg.append(int(p[2])/1000)
+            rev.append(int(p[3])/1000)
+
+    plt.plot(nobjs, disg, color='red', linestyle='--', marker="*", label="Disguise")
+    plt.plot(nobjs, rev, color='blue', marker="x", label="Reveal")
+    plt.xlabel("#Objects Associated with User")
+    plt.ylabel("Latency (ms)")
+    plt.legend()
+    plt.tight_layout(h_pad=4)
+    plt.savefig('{}.png'.format(test), dpi=300)
--- decor/benchmarks/run_tests.sh
+++ decor/benchmarks/run_tests.sh
@@ -7,13 +7,13 @@ trials=1
 #tests=( "no_shim" "decor" "shim_only" "shim_parse" )
 #tests=( "decor" )
 
-#cargo build --release
-cargo build
+cargo build --release
+#cargo build
 
 for trial in `seq $trials`
 do
 	perflock ../target/release/lobsters \
-		--scale=0.4 --nqueries=10000 --prop_unsub=0.0 #--prime
+		--scale=0.5 --nqueries=100 --prop_unsub=1.0 --prime
 done
 
 python3 plot.py
