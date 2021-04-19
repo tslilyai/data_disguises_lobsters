f57b861345910db223bcb37d070b018f0ec1a5f4
Lily Tsai <tslilyai@mit.edu>
2021-04-19 18:00:39 -0400

    move out of nested modules

---
+++ decor/src/decor/src/types.rs
@@ -0,0 +1,20 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct RowVal {
+    pub column: String,
+    pub value: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct FK {
+    pub referencer_col: String,
+    pub fk_name: String,
+    pub fk_col: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct TableFKs {
+    pub name: String,
+    pub fks: Vec<FK>,
+}
---
+++ decor/src/decor/src/vault.rs
@@ -0,0 +1,367 @@
+use crate::stats::QueryStat;
+use crate::types::*;
+use crate::helpers::*;
+use mysql::prelude::*;
+use serde::Serialize;
+use sql_parser::ast::*;
+use std::str::FromStr;
+
+pub const VAULT_TABLE: &'static str = "VaultTable";
+pub const INSERT_GUISE: u64 = 0;
+pub const DELETE_GUISE: u64 = 1;
+pub const UPDATE_GUISE: u64 = 2;
+
+#[derive(Default)]
+pub struct VaultEntry {
+    pub vault_id: u64,
+    pub disguise_id: u64,
+    pub user_id: u64,
+    pub guise_name: String,
+    pub guise_id: u64,
+    pub referencer_name: String,
+    pub update_type: u64,
+    pub modified_cols: Vec<String>,
+    pub old_value: Vec<RowVal>,
+    pub new_value: Vec<RowVal>,
+    pub reversed: bool,
+}
+
+fn get_vault_entries_with_constraint(
+    constraint: Expr,
+    txn: &mut mysql::Transaction,
+    stats: &mut QueryStat,
+) -> Result<Vec<VaultEntry>, mysql::Error> {
+    let rows = get_query_rows_txn(&select_statement(VAULT_TABLE, Some(constraint)), txn, stats)?;
+    let mut ves = vec![];
+    for row in rows {
+        let mut ve: VaultEntry = Default::default();
+        for rv in row {
+            match rv.column.as_str() {
+                "vaultID" => ve.vault_id = u64::from_str(&rv.value).unwrap(),
+                "disguiseID" => ve.disguise_id = u64::from_str(&rv.value).unwrap(),
+                "userID" => ve.user_id = u64::from_str(&rv.value).unwrap(),
+                "guiseName" => ve.guise_name = rv.value.clone(),
+                "guiseID" => ve.guise_id = u64::from_str(&rv.value).unwrap(),
+                "referencerName" => ve.referencer_name = rv.value.clone(),
+                "updateType" => ve.update_type = u64::from_str(&rv.value).unwrap(),
+                "modifiedCols" => {
+                    ve.modified_cols = if rv.value != Value::Null.to_string() {
+                        serde_json::from_str(&rv.value).unwrap()
+                    } else {
+                        vec![]
+                    }
+                }
+                "oldValue" => {
+                    ve.old_value = if rv.value != Value::Null.to_string() {
+                        serde_json::from_str(&rv.value).unwrap()
+                    } else {
+                        vec![]
+                    }
+                }
+                "newValue" => {
+                    ve.new_value = if rv.value != Value::Null.to_string() {
+                        serde_json::from_str(&rv.value).unwrap()
+                    } else {
+                        vec![]
+                    }
+                }
+                "reversed" => ve.reversed = rv.value != "0",
+                _ => unimplemented!("Incorrect column name! {:?}", rv),
+            };
+        }
+        ves.push(ve);
+    }
+    Ok(ves)
+}
+
+fn vec_to_expr<T: Serialize>(vs: &Vec<T>) -> Expr {
+    if vs.is_empty() {
+        Expr::Value(Value::Null)
+    } else {
+        let serialized = serde_json::to_string(&vs).unwrap();
+        Expr::Value(Value::String(serialized))
+    }
+}
+
+pub fn mark_vault_entry_reversed(
+    ve: &VaultEntry,
+    txn: &mut mysql::Transaction,
+    stats: &mut QueryStat,
+) -> Result<(), mysql::Error> {
+    get_query_rows_txn(
+        &Statement::Update(UpdateStatement {
+            table_name: string_to_objname(&VAULT_TABLE),
+            assignments: vec![Assignment {
+                id: Ident::new("reversed"),
+                value: Expr::Value(Value::Boolean(true)),
+            }],
+            selection: Some(Expr::BinaryOp {
+                left: Box::new(Expr::Identifier(vec![Ident::new("vaultID")])),
+                op: BinaryOperator::Eq,
+                right: Box::new(Expr::Value(Value::Number(ve.vault_id.to_string()))),
+            }),
+        }),
+        txn,
+        stats,
+    )?;
+    Ok(())
+}
+
+pub fn get_user_entries_with_referencer_in_vault(
+    uid: u64,
+    referencer_table: &str,
+    is_reversed: bool,
+    txn: &mut mysql::Transaction,
+    stats: &mut QueryStat,
+) -> Result<Vec<VaultEntry>, mysql::Error> {
+    let equal_uid_constraint = Expr::BinaryOp {
+        left: Box::new(Expr::Identifier(vec![Ident::new("userID")])),
+        op: BinaryOperator::Eq,
+        right: Box::new(Expr::Value(Value::Number(uid.to_string()))),
+    };
+    let reversed_constraint = match is_reversed {
+        true => Expr::Identifier(vec![Ident::new("reversed")]),
+        false => Expr::UnaryOp {
+            op: UnaryOperator::Not,
+            expr: Box::new(Expr::Identifier(vec![Ident::new("reversed")])),
+        },
+    };
+    let ref_constraint = Expr::BinaryOp {
+        left: Box::new(Expr::Identifier(vec![Ident::new("referencerName")])),
+        op: BinaryOperator::Eq,
+        right: Box::new(Expr::Value(Value::String(referencer_table.to_string()))),
+    };
+    let intermed_constraint = Expr::BinaryOp {
+        left: Box::new(equal_uid_constraint),
+        op: BinaryOperator::And,
+        right: Box::new(reversed_constraint),
+    };
+    let final_constraint = Expr::BinaryOp {
+        left: Box::new(intermed_constraint),
+        op: BinaryOperator::And,
+        right: Box::new(ref_constraint),
+    };
+    get_vault_entries_with_constraint(final_constraint, txn, stats)
+}
+
+pub fn get_user_entries_of_table_in_vault(
+    uid: u64,
+    guise_table: &str,
+    is_reversed: bool,
+    txn: &mut mysql::Transaction,
+    stats: &mut QueryStat,
+) -> Result<Vec<VaultEntry>, mysql::Error> {
+    let equal_uid_constraint = Expr::BinaryOp {
+        left: Box::new(Expr::Identifier(vec![Ident::new("userID")])),
+        op: BinaryOperator::Eq,
+        right: Box::new(Expr::Value(Value::Number(uid.to_string()))),
+    };
+    let reversed_constraint = match is_reversed {
+        true => Expr::Identifier(vec![Ident::new("reversed")]),
+        false => Expr::UnaryOp {
+            op: UnaryOperator::Not,
+            expr: Box::new(Expr::Identifier(vec![Ident::new("reversed")])),
+        },
+    };
+    let g_constraint = Expr::BinaryOp {
+        left: Box::new(Expr::Identifier(vec![Ident::new("guiseName")])),
+        op: BinaryOperator::Eq,
+        right: Box::new(Expr::Value(Value::String(guise_table.to_string()))),
+    };
+    let intermed_constraint = Expr::BinaryOp {
+        left: Box::new(equal_uid_constraint),
+        op: BinaryOperator::And,
+        right: Box::new(reversed_constraint),
+    };
+    let final_constraint = Expr::BinaryOp {
+        left: Box::new(intermed_constraint),
+        op: BinaryOperator::And,
+        right: Box::new(g_constraint),
+    };
+    get_vault_entries_with_constraint(final_constraint, txn, stats)
+}
+
+pub fn insert_vault_entries(
+    entries: &Vec<VaultEntry>,
+    txn: &mut mysql::Transaction,
+    stats: &mut QueryStat,
+) -> Result<(), mysql::Error> {
+    let vault_vals: Vec<Vec<Expr>> = entries
+        .iter()
+        .map(|ve| {
+            let mut evals = vec![];
+            evals.push(Expr::Value(Value::Number(ve.disguise_id.to_string())));
+            evals.push(Expr::Value(Value::Number(ve.user_id.to_string())));
+            evals.push(Expr::Value(Value::String(ve.guise_name.clone())));
+            evals.push(Expr::Value(Value::Number(ve.guise_id.to_string())));
+            evals.push(Expr::Value(Value::String(ve.referencer_name.clone())));
+            evals.push(Expr::Value(Value::Number(ve.update_type.to_string())));
+            evals.push(vec_to_expr(&ve.modified_cols));
+            evals.push(vec_to_expr(&ve.old_value));
+            evals.push(vec_to_expr(&ve.new_value));
+            evals.push(Expr::Value(Value::Boolean(ve.reversed)));
+            evals
+        })
+        .collect();
+    if !vault_vals.is_empty() {
+        get_query_rows_txn(
+            &Statement::Insert(InsertStatement {
+                table_name: string_to_objname(VAULT_TABLE),
+                columns: get_insert_vault_colnames(),
+                source: InsertSource::Query(Box::new(values_query(vault_vals))),
+            }),
+            txn,
+            stats,
+        )?;
+    }
+    Ok(())
+}
+
+pub fn get_insert_vault_colnames() -> Vec<Ident> {
+    vec![
+        Ident::new("disguiseID"),
+        Ident::new("userID"),
+        Ident::new("guiseName"),
+        Ident::new("guiseID"),
+        Ident::new("referencerName"),
+        Ident::new("updateType"),   // remove, add, or modify
+        Ident::new("modifiedCols"), // null if all modified
+        Ident::new("oldValue"),
+        Ident::new("newValue"),
+        Ident::new("reversed"),
+    ]
+}
+
+pub fn get_vault_cols() -> Vec<ColumnDef> {
+    vec![
+        // for ordering
+        ColumnDef {
+            name: Ident::new("vaultID"),
+            data_type: DataType::BigInt,
+            collation: None,
+            options: vec![
+                ColumnOptionDef {
+                    name: None,
+                    option: ColumnOption::NotNull,
+                },
+                ColumnOptionDef {
+                    name: None,
+                    option: ColumnOption::AutoIncrement,
+                },
+                ColumnOptionDef {
+                    name: None,
+                    option: ColumnOption::Unique { is_primary: true },
+                },
+            ],
+        },
+        // FK to disguise history
+        ColumnDef {
+            name: Ident::new("disguiseID"),
+            data_type: DataType::BigInt,
+            collation: None,
+            options: vec![ColumnOptionDef {
+                name: None,
+                option: ColumnOption::NotNull,
+            }],
+        },
+        // user ID
+        ColumnDef {
+            name: Ident::new("userID"),
+            data_type: DataType::BigInt,
+            collation: None,
+            options: vec![],
+        },
+        // table and column name
+        ColumnDef {
+            name: Ident::new("guiseName"),
+            data_type: DataType::Varbinary(4096),
+            collation: None,
+            options: vec![],
+        },
+        // guise ID
+        ColumnDef {
+            name: Ident::new("guiseID"),
+            data_type: DataType::BigInt,
+            collation: None,
+            options: vec![ColumnOptionDef {
+                name: None,
+                option: ColumnOption::NotNull,
+            }],
+        },
+        // optional name of referencer
+        ColumnDef {
+            name: Ident::new("referencerName"),
+            data_type: DataType::Varbinary(4096),
+            collation: None,
+            options: vec![],
+        },
+        // type of vault entry
+        ColumnDef {
+            name: Ident::new("updateType"),
+            data_type: DataType::Int,
+            collation: None,
+            options: vec![],
+        },
+        // modified columns, null if all modified
+        ColumnDef {
+            name: Ident::new("modifiedCols"),
+            data_type: DataType::Varbinary(4096),
+            collation: None,
+            options: vec![],
+        },
+        // value that object was changed from
+        ColumnDef {
+            name: Ident::new("oldValue"),
+            data_type: DataType::Varbinary(4096),
+            collation: None,
+            options: vec![],
+        },
+        // value that object was changed from
+        ColumnDef {
+            name: Ident::new("newValue"),
+            data_type: DataType::Varbinary(4096),
+            collation: None,
+            options: vec![],
+        },
+        // whether this update has been reversed
+        ColumnDef {
+            name: Ident::new("reversed"),
+            data_type: DataType::Boolean,
+            collation: None,
+            options: vec![],
+        },
+    ]
+}
+
+pub fn create_vault(in_memory: bool, txn: &mut mysql::Transaction) -> Result<(), mysql::Error> {
+    let engine = Some(if in_memory {
+        Engine::Memory
+    } else {
+        Engine::InnoDB
+    });
+    let indexes = vec![
+        IndexDef {
+            name: Ident::new("uidIndex"),
+            index_type: None,
+            key_parts: vec![Ident::new("userID")],
+        },
+        IndexDef {
+            name: Ident::new("disguiseIndex"),
+            index_type: None,
+            key_parts: vec![Ident::new("disguiseID")],
+        },
+    ];
+
+    txn.query_drop(
+        &Statement::CreateTable(CreateTableStatement {
+            name: string_to_objname(VAULT_TABLE),
+            columns: get_vault_cols(),
+            constraints: vec![],
+            indexes: indexes.clone(),
+            with_options: vec![],
+            if_not_exists: true,
+            engine: engine.clone(),
+        })
+        .to_string(),
+    )
+}
