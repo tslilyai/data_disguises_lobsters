06ca38e15a83fc9f75168cf1e85a515769d63cce
Lily Tsai <tslilyai@mit.edu>
2021-06-10 08:50:59 -0400

    implement querying from S3

--- decor/src/decor/src/uvclient.rs
+++ decor/src/decor/src/uvclient.rs
@@ -1,15 +1,11 @@
 use crate::vault;
 use crypto::{aead::*, chacha20poly1305::*};
+use futures::executor;
 use rusoto_core::request::HttpClient;
 use rusoto_core::{ByteStream, Region};
 use rusoto_credential::ProfileProvider;
-use futures::executor;
 use rusoto_s3::{
-    GetObjectOutput,
-    //CompleteMultipartUploadOutput, GetObjectRequest, PutObjectOutput,
-    PutObjectRequest,
-    S3Client,
-    S3,
+    GetObjectOutput, GetObjectRequest, ListObjectsV2Request, PutObjectRequest, S3Client, S3,
 };
 use serde::{Deserialize, Serialize};
 use std::io::Read;
@@ -56,15 +52,18 @@ impl UVClient {
         let mut tag = vec![];
         chacha.encrypt(&plaintxt, &mut encrypted, &mut tag);
 
+        // Key will look like UID/table/update_type, VEID
         let uvobj = UVObject {
-            key: format!("{}/{}", uid, ve.vault_id),
+            key: format!(
+                "{}/{}/{}/{}",
+                ve.user_id, ve.guise_name, ve.update_type, ve.vault_id
+            ),
             body: encrypted,
             tag: tag,
         };
         let serialized = serde_json::to_string(&uvobj).unwrap();
 
         // insert into user's s3 bucket
-        // key will look like UID/VEID
         let put_req = PutObjectRequest {
             acl: None,
             body: Some(ByteStream::from(serialized.as_bytes().to_vec())),
@@ -105,14 +104,81 @@ impl UVClient {
         }
     }
 
-    pub fn get_ves_of_user(
+    pub fn get_ves(
         &mut self,
         uid: u64,
+        table: Option<String>,
+        update_type: Option<u64>,
         ukey: &[u8],
         nonce: &[u8],
     ) -> Vec<vault::VaultEntry> {
         // read objects of user's s3 bucket
-        let objs: Vec<GetObjectOutput> = vec![];
+        let key_prefix = match table {
+            Some(tab) => match update_type {
+                Some(ut) => format!("{}/{}/{}/", uid, tab, ut),
+                None => format!("{}/{}/", uid, tab),
+            },
+            None => format!("{}/", uid),
+        };
+
+        let mut objs: Vec<GetObjectOutput> = vec![];
+        let mut ct = None;
+        let mut started = false;
+        while !ct.is_none() || !started {
+            let list_req = ListObjectsV2Request {
+                bucket: BUCKET.to_string(),
+                continuation_token: ct.clone(),
+                delimiter: None,
+                encoding_type: None,
+                expected_bucket_owner: None,
+                fetch_owner: None,
+                max_keys: None,
+                prefix: Some(key_prefix.clone()),
+                request_payer: None,
+                start_after: None,
+            };
+            match executor::block_on(self.s3client.list_objects_v2(list_req)) {
+                Ok(output) => {
+                    ct = output.next_continuation_token;
+                    let metaobjs = output.contents.unwrap();
+                    let mut get_req = GetObjectRequest {
+                        bucket: BUCKET.to_string(),
+                        expected_bucket_owner: None,
+                        if_match: None,
+                        if_modified_since: None,
+                        if_none_match: None,
+                        if_unmodified_since: None,
+                        key: String::new(),
+                        part_number: None,
+                        range: None,
+                        request_payer: None,
+                        response_cache_control: None,
+                        response_content_disposition: None,
+                        response_content_encoding: None,
+                        response_content_language: None,
+                        response_content_type: None,
+                        response_expires: None,
+                        sse_customer_algorithm: None,
+                        sse_customer_key: None,
+                        sse_customer_key_md5: None,
+                        version_id: None,
+                    };
+                    for o in metaobjs {
+                        get_req.key = o.key.unwrap();
+                        match executor::block_on(self.s3client.get_object(get_req.clone())) {
+                            Ok(obj) => objs.push(obj),
+                            Err(e) => {
+                                unimplemented!("Failed to get obj for {} to S3 bucket: {}", uid, e)
+                            }
+                        }
+                    }
+                }
+                Err(e) => unimplemented!("Failed to add ve for {} to S3 bucket: {}", uid, e),
+            }
+            if !started {
+                started = true;
+            }
+        }
 
         let mut ves = vec![];
         let mut chacha = ChaCha20Poly1305::new(ukey, nonce, &vec![]);
