0240decfbbd9c801d32f2407f3c2f890c7d12501
Malte Schwarzkopf <malte@cs.brown.edu>
2021-09-06 17:54:10 -0400

    Set up Rocket MySQL DB connector in demo app

--- demo-app/src/main.rs
+++ demo-app/src/main.rs
@@ -1,4 +1,10 @@
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_sync_db_pools;
+
+use rocket_sync_db_pools::{diesel, database};
+
+#[database("mysql_db")]
+struct DbConn(diesel::MysqlConnection);
 
 #[get("/")]
 fn index() -> &'static str {
@@ -7,5 +13,7 @@ fn index() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .attach(DbConn::fairing())
+        .mount("/", routes![index])
 }
