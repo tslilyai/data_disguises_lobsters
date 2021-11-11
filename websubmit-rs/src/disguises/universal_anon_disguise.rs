1167341b2ba14803802cc0f91c2b12543c2608f6
Lily Tsai <tslilyai@mit.edu>
2021-11-11 11:12:42 -0500

    batch db ops in anonymization, deal with extra quotes again...

--- websubmit-rs/src/disguises/universal_anon_disguise.rs
+++ websubmit-rs/src/disguises/universal_anon_disguise.rs
@@ -2,7 +2,15 @@ use crate::backend::MySqlBackend;
 use edna::*;
 use mysql::from_value;
 use mysql::prelude::*;
+use mysql::*;
 use std::collections::HashMap;
+
+struct UpdateFK {
+    new_uid: String,
+    user: String,
+    lec: u64,
+    q: u64,
+}
 
 pub fn get_did() -> DID {
     1
@@ -10,19 +18,16 @@ pub fn get_did() -> DID {
 
 pub fn apply(
     bg: &mut MySqlBackend,
-) -> Result<
-    (
-        HashMap<(UID, DID), tokens::LocCap>,
-        HashMap<(UID, DID), tokens::LocCap>,
-    ),
-    mysql::Error,
-> {
+) -> Result<(
+    HashMap<(UID, DID), tokens::LocCap>,
+    HashMap<(UID, DID), tokens::LocCap>,
+)> {
     // DECOR ANSWERS
     bg.edna.start_disguise(get_did());
 
     // get all answers sorted by user and lecture
     let mut user_lec_answers: HashMap<(String, u64), Vec<u64>> = HashMap::new();
-    flame::start("get_answers");
+    flame::start("DB: get_answers");
     let res = bg.query_exec("all_answers", vec![]);
     for r in res {
         let uid: String = from_value(r[0].clone());
@@ -36,45 +41,68 @@ pub fn apply(
             }
         };
     }
-    flame::end("get_answers");
+    flame::end("DB: get_answers");
 
+    let mut users = vec![];
+    let mut updates = vec![];
     for ((user, lecture), qs) in user_lec_answers {
         // insert a new pseudoprincipal
-        flame::start("create_pseudoprincipal");
+        flame::start("EDNA: create_pseudoprincipal");
         let (new_uid, rowvals) = bg.edna.create_new_pseudoprincipal();
-        flame::end("create_pseudoprincipal");
+        flame::end("EDNA: create_pseudoprincipal");
 
         // XXX issue where using bg adds quotes everywhere...
-        flame::start("insert_pseudoprincipal");
-        let q = format!(
-            r"INSERT INTO {} VALUES ({});",
-            "users",
+        users.push(
             rowvals
                 .iter()
                 .map(|rv| rv.value.clone())
-                .collect::<Vec<String>>()
-                .join(",")
+                .collect::<Vec<String>>(),
         );
-        bg.handle.query_drop(q).unwrap();
-        flame::end("insert_pseudoprincipal");
 
         // rewrite answers for all qs to point from user to new pseudoprincipal
-        flame::start("update_answers");
         for q in qs {
-            let q = format!(
-                r"UPDATE {} SET `user` = {} WHERE `user` = '{}' AND lec = {} AND q = {};",
-                "answers", new_uid, user, lecture, q
-            );
-            bg.handle.query_drop(q).unwrap();
+            updates.push(UpdateFK {
+                new_uid: new_uid.clone(),
+                user: user.clone(),
+                lec: lecture,
+                q: q,
+            });
         }
-        flame::end("update_answers");
 
         // register new ownershiptoken for pseudoprincipal
-        flame::start("save_pseudoprincipal");
+        flame::start("ENDA: save_pseudoprincipal");
         bg.edna
             .save_pseudoprincipal_token(get_did(), user, new_uid, vec![]);
-        flame::end("save_pseudoprincipal");
+        flame::end("ENDA: save_pseudoprincipal");
     }
+
+    flame::start("DB: insert pseudos");
+    bg.handle.exec_batch(
+        r"INSERT INTO `users` VALUES (:email, :apikey, :is_admin, :is_anon);",
+        users.iter().map(|u| {
+            params! {
+                "email" => &u[0].trim_matches('\''),
+                "apikey" => &u[1].trim_matches('\''),
+                "is_admin" => &u[2],
+                "is_anon" => &u[3],
+            }
+        }),
+    )?;
+    flame::end("DB: insert pseudos");
+
+    flame::start("DB: update_answers");
+    bg.handle.exec_batch(
+        r"UPDATE answers SET `user` = :newuid WHERE `user` = :user AND lec = :lec AND q = :q;",
+        updates.iter().map(|u| {
+            params! {
+                "newuid" => &u.new_uid.trim_matches('\''),
+                "user" => &u.user.trim_matches('\''),
+                "lec" => u.lec,
+                "q" => u.q,
+            }
+        }),
+    )?;
+    flame::end("DB: update_answers");
 
     Ok(bg.edna.end_disguise(get_did()))
 }
