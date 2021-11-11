c894cb6a72dea42a169a787b728d8f242e23556c
Lily Tsai <tslilyai@mit.edu>
2021-11-10 20:30:58 -0500

    update plot and run_benchmarks; plotting works

--- websubmit-rs/plot.py
+++ websubmit-rs/plot.py
@@ -1,68 +1,58 @@
-import numpy as np
 import matplotlib.pyplot as plt
 import csv
 import statistics
 
 plt.style.use('seaborn-deep')
-
-tests = ["decor", "shim_only", "shim_parse"]
-names = ["Read", "Update", "Insert", "Unsub", "Resub", "Delete"]
-ybounds = [1000000, 1000000, 1000000, 10000, 6000, 6000]
-
-nlec = [20, 40, 60]
-nusers = [10, 30, 50, 70]
+#nlec = [10, 20, 30]
+nlec = [20]
+nusers = [5, 10, 15, 20, 25, 30]#, 50, 70]
 
 # collect all results, compute maximum latency over all tests + all query  types
 for l in nlec:
-    let account_results = []
-    let anon_results = []
-    let edit_results = []
-    let delete_results = []
-    let restore_results = []
+    account_results = []
+    anon_results = []
+    edit_results = []
+    delete_results = []
+    restore_results = []
 
     for u in nusers:
         with open('disguise_stats_{}lec_{}users.csv'.format(l, u),'r') as csvfile:
             rows = csvfile.readlines()
-            let account_durs = [int(x) for x in rows[0].strip().split(',')]
-            let anon_durs = [int(x) for x in rows[1].strip().split(',')]
-            let edit_durs = [int(x) for x in rows[2].strip().split(',')]
-            let delete_durs = [int(x) for x in rows[3].strip().split(',')]
-            let restore_durs = [int(x) for x in rows[4].strip().split(',')]
+            account_durs = [int(x) for x in rows[0].strip().split(',')]
+            anon_durs = [int(x) for x in rows[1].strip().split(',')]
+            edit_durs = [int(x) for x in rows[2].strip().split(',')]
+            delete_durs = [int(x) for x in rows[3].strip().split(',')]
+            restore_durs = [int(x) for x in rows[4].strip().split(',')]
 
-            account_results.push(statistics.mean(account_durs))
-            anon_results.push(statistics.mean(anon_durs))
-            edit_results.push(statistics.mean(edit_durs))
-            delete_results.push(statistics.mean(delete_durs))
-            restore_results.push(statistics.mean(restore_durs))
+            account_results.append(statistics.mean(account_durs))
+            anon_results.append(statistics.mean(anon_durs))
+            edit_results.append(statistics.mean(edit_durs))
+            delete_results.append(statistics.mean(delete_durs))
+            restore_results.append(statistics.mean(restore_durs))
 
-    fig, axes = plt.subplots(nrows=len(nusers), ncols=1, figsize=(8,15))
+    fig, axes = plt.subplots(nrows=5, ncols=1, figsize=(8,15))
     axes_flat = axes.flatten()
     axes_flat[0].plot(nusers, account_results)
-    axes_flat[0].legend(loc='upper right')
     axes_flat[0].set_title("Average Time to Create Account")
     axes_flat[0].set_xlabel('Number of users')
     axes_flat[0].set_ylabel('Time (ms)')
 
     axes_flat[1].plot(nusers, anon_results)
-    axes_flat[1].legend(loc='upper right')
     axes_flat[1].set_title("Time to Anonymize All Accounts")
     axes_flat[1].set_xlabel('Number of users')
     axes_flat[1].set_ylabel('Time (ms)')
 
     axes_flat[2].plot(nusers, edit_results)
-    axes_flat[2].legend(loc='upper right')
     axes_flat[2].set_title("Average Time to Edit Answers to Lecture")
     axes_flat[2].set_xlabel('Number of users')
     axes_flat[2].set_ylabel('Time (ms)')
 
     axes_flat[3].plot(nusers, delete_results)
-    axes_flat[3].legend(loc='upper right')
     axes_flat[3].set_title("Average Time to Delete Account")
     axes_flat[3].set_xlabel('Number of users')
     axes_flat[3].set_ylabel('Time (ms)')
 
     axes_flat[4].plot(nusers, restore_results)
-    axes_flat[4].legend(loc='upper right')
     axes_flat[4].set_title("Average Time to Restore Account")
     axes_flat[4].set_xlabel('Number of users')
     axes_flat[4].set_ylabel('Time (ms)')
--- websubmit-rs/run_benchmarks.sh
+++ websubmit-rs/run_benchmarks.sh
@@ -3,8 +3,10 @@
 RUST_LOG=error
 cargo build --release
 
-for (( u = 1; u < 50 ; u+=2 )); do
-    for (( l = 1; l < 20; l+=5 )); do
-	RUST_LOG=error ../target/release/websubmit -i myclass --benchmark true --prime true --nusers $(( 2*u )) --nlec $(( 2*l )) --nqs 4
+for (( l = 20; l <= 20; l+=10 )); do
+    for (( u = 5; u <= 30; u+=5)); do
+	RUST_LOG=error ../target/release/websubmit -i myclass --benchmark true --prime true --nusers $u --nlec $l --nqs 4
     done
 done
+
+python3 plot.py
