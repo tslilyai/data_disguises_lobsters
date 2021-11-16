cbc4fc3cdbe0a8ebbba73f61280c2165fc67a971
Lily Tsai <tslilyai@mit.edu>
2021-11-15 19:14:53 -0500

    update plot and benchmark to also plot baseline

--- websubmit-rs/run_benchmarks.sh
+++ websubmit-rs/run_benchmarks.sh
@@ -8,6 +8,7 @@ for l in 20 40; do
 	RUST_LOG=error ../target/release/websubmit \
         -i myclass --benchmark true --prime true --nusers $u --nlec $l --nqs 4 --baseline &> \
             ${l}lec_${u}users_baseline.out
+	echo "Ran baseline test for $l lecture and $u users"
     RUST_LOG=error ../target/release/websubmit \
         -i myclass --benchmark true --prime true --nusers $u --nlec $l --nqs 4 &> \
             ${l}lec_${u}users.out
