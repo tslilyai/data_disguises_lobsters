e25a37637aec2b4249a2beea4511997089b4ae8f
tslilyai <tslilyai@mit.edu>
2021-11-17 20:58:07 -0500

    fix directories for run_benchmarks

--- applications/websubmit-rs/run_benchmarks.sh
+++ applications/websubmit-rs/run_benchmarks.sh
@@ -8,12 +8,12 @@ mkdir output
 for l in 20 40; do
     for u in 10 20 30 50 70 100; do
 	RUST_LOG=error perflock ../../target/release/websubmit-server \
-        -i myclass --benchmark true --prime true --nusers $u --nlec $l --nqs 4 --baseline true &> \
+        -i myclass --config server/sample-config.toml --benchmark true --prime true --nusers $u --nlec $l --nqs 4 --baseline true &> \
             output/${l}lec_${u}users_baseline.out
 	echo "Ran baseline test for $l lecture and $u users"
 
 	RUST_LOG=error perflock ../../target/release/websubmit-server \
-        -i myclass --benchmark true --prime true --nusers $u --nlec $l --nqs 4 --baseline false &> \
+        -i myclass --config server/sample-config.toml --benchmark true --prime true --nusers $u --nlec $l --nqs 4 --baseline false &> \
             output/${l}lec_${u}users.out
 	echo "Ran test for $l lecture and $u users"
     done
