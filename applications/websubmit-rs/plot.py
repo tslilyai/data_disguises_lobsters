04bb72b19eb42229fa194edba6016bd4ac554661
Lily Tsai <tslilyai@mit.edu>
2021-11-18 20:52:36 -0500

    update plotter

--- applications/websubmit-rs/plot.py
+++ applications/websubmit-rs/plot.py
@@ -3,13 +3,12 @@ import csv
 import statistics
 import sys
 from collections import defaultdict
-from itertools import cycle
 plt.style.use('seaborn-deep')
 
-nusers = [10, 30, 50]#, 70, 100]
-props = [1/10, 1/4, 1/2]
-maxts = 10000
-nbuckets = 100
+nusers = [10, 30]#, 50]#, 70, 100]
+props = [1/10, 1/4]#, 1/2]
+maxts = 100000
+nbuckets = 10000
 bucketwidth = maxts/nbuckets
 buckets = [b * bucketwidth for b in range(nbuckets)]
 
@@ -26,41 +25,49 @@ for u in nusers:
         with open('concurrent_disguise_stats_{}lec_{}users_{}disguisers.csv'.format(lec, u, nd),'r') as csvfile:
             rows = csvfile.readlines()
             editpairs = [x.split(':') for x in rows[0].strip().split(',')]
-            editdata = [[]] * nbuckets
+            editdata = defaultdict(list)
             for x in editpairs:
-                editdata[int((float(x[0])/1000)/bucketwidth)].append(float(x[1])/1000)
+                bucket = int((float(x[0]))/bucketwidth)
+                val = float(x[1])/1000
+                editdata[bucket].append(val)
 
             deletepairs = [x.split(':') for x in rows[1].strip().split(',')]
-            deletedata = [[]] * nbuckets
+            deletedata = defaultdict(list)
             for x in deletepairs:
-                deletedata[int((float(x[0])/1000)/bucketwidth)].append(float(x[1])/1000)
+                bucket = int((float(x[0]))/bucketwidth)
+                val = float(x[1])/1000
+                deletedata[bucket].append(val)
 
             restorepairs = [x.split(':') for x in rows[2].strip().split(',')]
-            restoredata = [[]] * nbuckets
+            restoredata = defaultdict(list)
             for x in restorepairs:
-                restoredata[int((float(x[0])/1000)/bucketwidth)].append(float(x[1])/1000)
+                bucket = int((float(x[0]))/bucketwidth)
+                val = float(x[1])/1000
+                restoredata[bucket].append(val)
 
-            edit_results[u].append([statistics.mean(x) for x in editdata])
-            delete_results[u].append([statistics.mean(x) for x in deletedata])
-            restore_results[u].append([statistics.mean(x) for x in restoredata])
+            edit_results[u].append(editdata)
+            delete_results[u].append(deletedata)
+            restore_results[u].append(restoredata)
 
         with open('concurrent_disguise_stats_{}lec_{}users_{}disguisers_baseline.csv'.format(lec, u, nd),'r') as csvfile:
             editpairs = [x.split(':') for x in rows[0].strip().split(',')]
-            editdata = [[]] * nbuckets
+            editdata = defaultdict(list)
             for x in editpairs:
-                editdata[int((float(x[0])/1000)/bucketwidth)].append(float(x[1])/1000)
-            edit_results_baseline[u].append([statistics.mean(x) for x in editdata])
+                bucket = int((float(x[0]))/bucketwidth)
+                val = float(x[1])/1000
+                editdata[bucket].append(val)
+            edit_results_baseline[u].append(editdata)
 
 for u in nusers:
     fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(8,15))
     axes_flat = axes.flatten()
-    axes_flat[0].plot(buckets, edit_results_baseline[u][0], label='baseline')
     for i in range(len(props)):
-        axes_flat[0].plot(buckets, edit_results[u][i], label='edna_{}'.format(props[i]))
+        axes_flat[0].scatter(edit_results[u][i].keys(), [statistics.mean(x) for x in edit_results[u][i].values()], label='edna_{}'.format(props[i]))
+    axes_flat[0].scatter(edit_results_baseline[u][0].keys(), [statistics.mean(x) for x in edit_results_baseline[u][0].values()], label='baseline')
     for i in range(len(props)):
-        axes_flat[1].plot(buckets, delete_results[u][i], label='edna_{}'.format(props[i]))
+        axes_flat[1].scatter(delete_results[u][i].keys(), [statistics.mean(x) for x in delete_results[u][i].values()], label='edna_{}'.format(props[i]))
     for i in range(len(props)):
-        axes_flat[2].plot(buckets, restore_results[u][i], label='edna_{}'.format(props[i]))
+        axes_flat[2].scatter(restore_results[u][i].keys(), [statistics.mean(x) for x in restore_results[u][i].values()], label='edna_{}'.format(props[i]))
 
     for i in range(len(axes_flat)):
         axes_flat[i].set_xlabel('Benchmark Time (ms)')
