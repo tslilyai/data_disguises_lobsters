148e61168ba6af62f49bd631bb4f87a1d3deefd0
Lily Tsai <tslilyai@mit.edu>
2021-11-26 06:14:26 -0500

    put all info on one graph

--- applications/websubmit-rs/plot.py
+++ applications/websubmit-rs/plot.py
@@ -4,6 +4,7 @@ import csv
 import statistics
 import sys
 from collections import defaultdict
+
 plt.style.use('seaborn-deep')
 
 nusers = [100]
@@ -15,13 +16,12 @@ buckets = [b * bucketwidth for b in range(nbuckets)]
 
 lec = sys.argv[1]
 
-
 # collect all results, compute maximum latency over all tests + all query  types
 normal_edit_results = defaultdict(list)
 edit_results = defaultdict(list)
+edit_results_baseline = defaultdict(list)
 delete_results = defaultdict(list)
 restore_results = defaultdict(list)
-edit_results_baseline = defaultdict(list)
 
 for u in nusers:
     with open('concurrent_disguise_stats_{}lec_{}users_disguising.csv'.format(lec, u),'r') as csvfile:
@@ -63,6 +63,7 @@ for u in nusers:
             restore_results[u].append(restoredata)
 
         with open('concurrent_disguise_stats_{}lec_{}users_{}disguisers_baseline.csv'.format(lec, u, nd),'r') as csvfile:
+            rows = csvfile.readlines()
             editpairs = [x.split(':') for x in rows[0].strip().split(',')]
             editdata = defaultdict(list)
             for x in editpairs:
@@ -71,47 +72,40 @@ for u in nusers:
                 editdata[bucket].append(val)
             edit_results_baseline[u].append(editdata)
 
-for u in nusers:
-    fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(8,15))
-    axes_flat = axes.flatten()
-    xs = list(edit_results_baseline[u][0].keys())
+xs = list(edit_results_baseline[100][0].keys())
+order = np.argsort(xs)
+xs = np.array(xs)[order]
+ys = [statistics.mean(x) for x in edit_results_baseline[100][0].values()]
+ys = np.array(ys)[order]
+plt.plot(xs, ys, label='0 Disguisers', linestyle="--", color='m')
+
+xs = list(normal_edit_results[100].keys())
+order = np.argsort(xs)
+xs = np.array(xs)[order]
+ys = [statistics.mean(x) for x in normal_edit_results[100].values()]
+ys = np.array(ys)[order]
+plt.plot(xs, ys, label='1 Disguisers', color='k')
+
+for i in range(len(props)):
+    xs = list(edit_results[100][i].keys())
     order = np.argsort(xs)
     xs = np.array(xs)[order]
-    ys = [statistics.mean(x) for x in edit_results_baseline[u][0].values()]
+    ys = [statistics.mean(x) for x in edit_results[100][i].values()]
     ys = np.array(ys)[order]
-    axes_flat[0].plot(xs, ys)
+    plt.plot(xs, ys, label='{} Disguisers'.format(int(props[i]*100)))
 
-    xs = list(normal_edit_results[u].keys())
-    order = np.argsort(xs)
-    xs = np.array(xs)[order]
-    ys = [statistics.mean(x) for x in normal_edit_results[u].values()]
-    ys = np.array(ys)[order]
-    axes_flat[1].plot(xs, ys)
+#for i in range(len(props)):
+    #axes_flat[2].scatter(delete_results[100][i].keys(), [statistics.mean(x) for x in delete_results[100][i].values()], label='edna_{}'.format(props[i]))
+#for i in range(len(props)):
+    #axes_flat[3].scatter(restore_results[100][i].keys(), [statistics.mean(x) for x in restore_results[100][i].values()], label='edna_{}'.format(props[i]))
 
-    for i in range(len(props)):
-        xs = list(edit_results[u][i].keys())
-        order = np.argsort(xs)
-        xs = np.array(xs)[order]
-        ys = [statistics.mean(x) for x in edit_results[u][i].values()]
-        ys = np.array(ys)[order]
-        axes_flat[2].plot(xs, ys, label='{} Disguisers'.format(props[i]*u))
+plt.xlabel('Benchmark Time (s)')
+plt.ylabel('Latency (ms)')
+plt.ylim(ymin=0)
+plt.xlim(xmin=0, xmax=nbuckets)
 
-    #for i in range(len(props)):
-        #axes_flat[2].scatter(delete_results[u][i].keys(), [statistics.mean(x) for x in delete_results[u][i].values()], label='edna_{}'.format(props[i]))
-    #for i in range(len(props)):
-        #axes_flat[3].scatter(restore_results[u][i].keys(), [statistics.mean(x) for x in restore_results[u][i].values()], label='edna_{}'.format(props[i]))
+plt.title("Edit Latency vs. Amount of Concurrent Disguising Actions")
+plt.legend(loc="best")
 
-    for i in range(len(axes_flat)):
-        axes_flat[i].set_xlabel('Benchmark Time (s)')
-        axes_flat[i].set_ylabel('Latency (ms)')
-        axes_flat[i].set_ylim(ymin=0)
-        axes_flat[i].set_xlim(xmin=0, xmax=nbuckets)
-
-    axes_flat[0].set_title("Baseline Latency")
-    axes_flat[1].set_title("Latency with Normal Disguising")
-    axes_flat[2].set_title("Latency with Worst-Case Disguising")
-    axes_flat[2].legend(loc="upper left")
-
-    fig.tight_layout(h_pad=4)
-    plt.savefig('concurrent_results_{}lec_{}users.png'.format(lec, u), dpi=300)
-    plt.clf()
+plt.tight_layout(h_pad=4)
+plt.savefig('concurrent_results_{}lec_{}users.pdf'.format(lec, 100))
