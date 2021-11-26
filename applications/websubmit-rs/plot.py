9c6f7f131b6627f208c5f03ac16c7b7927692a99
Lily Tsai <tslilyai@mit.edu>
2021-11-25 22:09:01 -0500

    add very basic description of concurrent editing and disguising benchmark

--- applications/websubmit-rs/plot.py
+++ applications/websubmit-rs/plot.py
@@ -6,7 +6,7 @@ import sys
 from collections import defaultdict
 plt.style.use('seaborn-deep')
 
-nusers = [100, 200]
+nusers = [100]
 props = [1/10, 1/4]
 maxts = 150000
 bucketwidth = 1000
@@ -15,13 +15,25 @@ buckets = [b * bucketwidth for b in range(nbuckets)]
 
 lec = sys.argv[1]
 
+
 # collect all results, compute maximum latency over all tests + all query  types
+normal_edit_results = defaultdict(list)
 edit_results = defaultdict(list)
 delete_results = defaultdict(list)
 restore_results = defaultdict(list)
 edit_results_baseline = defaultdict(list)
 
 for u in nusers:
+    with open('concurrent_disguise_stats_{}lec_{}users_disguising.csv'.format(lec, u),'r') as csvfile:
+        rows = csvfile.readlines()
+        editpairs = [x.split(':') for x in rows[0].strip().split(',')]
+        editdata = defaultdict(list)
+        for x in editpairs:
+            bucket = int((float(x[0]))/bucketwidth)
+            val = float(x[1])/1000
+            editdata[bucket].append(val)
+        normal_edit_results[u] = editdata
+
     for nd in [int(u * prop) for prop in props]:
         with open('concurrent_disguise_stats_{}lec_{}users_disguising_{}batch.csv'.format(lec, u, nd),'r') as csvfile:
             rows = csvfile.readlines()
@@ -60,39 +72,45 @@ for u in nusers:
             edit_results_baseline[u].append(editdata)
 
 for u in nusers:
-    fig, axes = plt.subplots(nrows=4, ncols=1, figsize=(8,15))
+    fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(8,15))
     axes_flat = axes.flatten()
-    for i in range(len(props)):
-        xs = list(edit_results_baseline[u][i].keys())
-        order = np.argsort(xs)
-        xs = np.array(xs)[order]
-        ys = [statistics.mean(x) for x in edit_results_baseline[u][0].values()]
-        ys = np.array(ys)[order]
-        axes_flat[0].plot(xs, ys, label='baseline_{}'.format(props[i]))
+    xs = list(edit_results_baseline[u][0].keys())
+    order = np.argsort(xs)
+    xs = np.array(xs)[order]
+    ys = [statistics.mean(x) for x in edit_results_baseline[u][0].values()]
+    ys = np.array(ys)[order]
+    axes_flat[0].plot(xs, ys)
 
+    xs = list(normal_edit_results[u].keys())
+    order = np.argsort(xs)
+    xs = np.array(xs)[order]
+    ys = [statistics.mean(x) for x in normal_edit_results[u].values()]
+    ys = np.array(ys)[order]
+    axes_flat[1].plot(xs, ys)
+
+    for i in range(len(props)):
         xs = list(edit_results[u][i].keys())
         order = np.argsort(xs)
         xs = np.array(xs)[order]
         ys = [statistics.mean(x) for x in edit_results[u][i].values()]
         ys = np.array(ys)[order]
-        axes_flat[1].plot(xs, ys, label='edna_{}'.format(props[i]))
+        axes_flat[2].plot(xs, ys, label='{} Disguisers'.format(props[i]*u))
 
-    for i in range(len(props)):
-        axes_flat[2].scatter(delete_results[u][i].keys(), [statistics.mean(x) for x in delete_results[u][i].values()], label='edna_{}'.format(props[i]))
-    for i in range(len(props)):
-        axes_flat[3].scatter(restore_results[u][i].keys(), [statistics.mean(x) for x in restore_results[u][i].values()], label='edna_{}'.format(props[i]))
+    #for i in range(len(props)):
+        #axes_flat[2].scatter(delete_results[u][i].keys(), [statistics.mean(x) for x in delete_results[u][i].values()], label='edna_{}'.format(props[i]))
+    #for i in range(len(props)):
+        #axes_flat[3].scatter(restore_results[u][i].keys(), [statistics.mean(x) for x in restore_results[u][i].values()], label='edna_{}'.format(props[i]))
 
     for i in range(len(axes_flat)):
         axes_flat[i].set_xlabel('Benchmark Time (s)')
         axes_flat[i].set_ylabel('Latency (ms)')
         axes_flat[i].set_ylim(ymin=0)
         axes_flat[i].set_xlim(xmin=0, xmax=nbuckets)
-        axes_flat[i].legend(loc='upper left');
 
-    axes_flat[0].set_title("Baseline: Editing Answers to Lecture Latency")
-    axes_flat[1].set_title("With Disguises: Editing Answers to Lecture Latency")
-    axes_flat[2].set_title("Delete Account Latency")
-    axes_flat[3].set_title("Restore Account Latency")
+    axes_flat[0].set_title("Baseline Latency")
+    axes_flat[1].set_title("Latency with Normal Disguising")
+    axes_flat[2].set_title("Latency with Worst-Case Disguising")
+    axes_flat[2].legend(loc="upper left")
 
     fig.tight_layout(h_pad=4)
     plt.savefig('concurrent_results_{}lec_{}users.png'.format(lec, u), dpi=300)
