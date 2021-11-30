cdce889fadd5fa04bb725d10fcaedbb9748fc8af
Lily Tsai <tslilyai@mit.edu>
2021-11-30 12:16:38 -0500

    writeup latency of disguise-related actions, include figure of bar graph in websubmit and hotcrp

--- applications/plot_stats.py
+++ applications/plot_stats.py
@@ -6,12 +6,12 @@ import numpy as np
 
 plt.style.use('seaborn-deep')
 
-fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(8,6))
+fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(8,8))
 axes_flat = axes.flatten()
 barwidth = 0.25
 # positions
 X = np.arange(5)
-labels = ['Create Account', 'Edit Lecture', 'Delete Account', 'Restore Account', 'Anonymize Account']
+labels = ['Create Account', 'Edit Data', 'Delete Account', 'Restore Account', 'Anonymize Account']
 
 # WEBSUBMIT RESULTS
 for (i, ax) in enumerate(axes_flat):
@@ -29,8 +29,8 @@ for (i, ax) in enumerate(axes_flat):
     edit_results_baseline = 0
     delete_results_baseline = 0
 
-    filename = "hotcrp_disguise_stats.csv"
-    filename_baseline = "hotcrp_disguise_stats_baseline.csv"
+    filename = "hotcrp_disguise_stats_450users.csv"
+    filename_baseline = "hotcrp_disguise_stats_450users_baseline.csv"
     title = "HotCRP Operation Latencies"
     if i == 0:
         filename = 'websubmit_{}lec_{}users_stats.csv'.format(20, 100)
@@ -68,16 +68,25 @@ for (i, ax) in enumerate(axes_flat):
         edit_results_baseline=statistics.mean(edit_durs)
         delete_results_baseline=statistics.mean(delete_durs)
 
-    rects1= ax.bar(X-barwidth, [account_results_baseline, edit_results_baseline, delete_results_baseline, 0,
-        anon_results_baseline], color='g', width=barwidth, label="Baseline")
-    rects2= ax.bar(X, [account_results, edit_results_noanon, delete_results_noanon, restore_results_noanon, 0],
-            color='b', width=barwidth, label="With Edna, Pre-Anonymization")
-    rects3=ax.bar(X+barwidth, [account_results, edit_results, delete_results, restore_results, anon_results],
-            color='r', width=barwidth, label="With Edna, Post-Anonymization")
+    # add baseline closer to red line for anonymize
+    ax.bar(X-barwidth, [account_results_baseline, edit_results_baseline, delete_results_baseline, 0,
+        0], color='g', width=barwidth, label="No Edna")
+    ax.bar(X-barwidth/2, [0, 0, 0, 0, anon_results_baseline], color='g', width=barwidth)
+
+    ax.bar(X, [account_results, edit_results_noanon, delete_results_noanon, 0, 0],
+            color='b', width=barwidth, label="Edna")
+    ax.bar(X-barwidth/2, [0, 0, 0, restore_results_noanon, 0], color='b', width=barwidth)
+    ax.bar(X+barwidth/2, [0, 0, 0, 0, anon_results], color='b', width=barwidth)
+
+    ax.bar(X+barwidth, [account_results, edit_results, delete_results, 0, 0],
+            color='r', width=barwidth, label="Edna + Temporary Recorrelation")
+    ax.bar(X+barwidth/2, [0, 0, 0, restore_results, 0], color='r', width=barwidth)
+
     ax.set_title(title)
     ax.set_ylabel('Time (ms)')
-    ax.set_ylim(ymin=0)
+    ax.set_ylim(ymin=0.01)
     ax.set_xticks(X)
+    ax.set_yscale('log')
     ax.set_xticklabels(labels)
     ax.legend(loc='best');
 
