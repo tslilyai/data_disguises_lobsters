005b74e5ff23610b1ee8e85bdfbfe4eb092463e4
tslilyai <tslilyai@mit.edu>
2021-11-30 18:04:08 -0500

    fix lobsters guise_gen

--- applications/lobsters/src/disguises/guise_gen.rs
+++ applications/lobsters/src/disguises/guise_gen.rs
@@ -7,62 +7,27 @@ use std::collections::HashMap;
 
 pub fn get_insert_guise_cols() -> Vec<String> {
     vec![
-        "contactId".to_string(),
-        "firstName".to_string(),
-        "lastName".to_string(),
-        "unaccentedName".to_string(),
-        "email".to_string(),
-        "preferredEmail".to_string(),
-        "affiliation".to_string(),
-        "phone".to_string(),
-        "country".to_string(),
-        "password".to_string(),
-        "passwordTime".to_string(),
-        "passwordUseTime".to_string(),
-        "collaborators".to_string(),
-        "updateTime".to_string(),
-        "lastLogin".to_string(),
-        "defaultWatch".to_string(),
-        "roles".to_string(),
-        "disabled".to_string(),
-        "contactTags".to_string(),
-        "data".to_string(),
+        "id".to_string(),
+        "username".to_string(),
+        "karma".to_string(),
     ]
 }
 
 pub fn get_insert_guise_vals() -> Vec<Expr> {
     let mut rng = rand::thread_rng();
     let gid: u64 = rng.gen::<u64>();
-    let email: String = format!("{}@anon.com", gid);
-    let pass: String = format!("{}pass", gid);
+    let username: String = format!("anon{}", gid);
     vec![
         Expr::Value(Value::Number(gid.to_string())),
-        Expr::Value(Value::String(String::new())),
-        Expr::Value(Value::String(String::new())),
-        Expr::Value(Value::String(String::new())),
-        Expr::Value(Value::String(email)),
-        Expr::Value(Value::Null),
-        Expr::Value(Value::String(String::new())),
-        Expr::Value(Value::Null),
-        Expr::Value(Value::Null),
-        Expr::Value(Value::String(pass)),
+        Expr::Value(Value::String(username)),
         Expr::Value(Value::Number(0.to_string())),
-        Expr::Value(Value::Number(0.to_string())),
-        Expr::Value(Value::Null),
-        Expr::Value(Value::Number(0.to_string())),
-        Expr::Value(Value::Number(0.to_string())),
-        Expr::Value(Value::Number(2.to_string())),
-        Expr::Value(Value::Number(0.to_string())),
-        Expr::Value(Value::Number(0.to_string())),
-        Expr::Value(Value::Null),
-        Expr::Value(Value::Null),
     ]
 }
 
 pub fn get_guise_gen() -> Arc<RwLock<GuiseGen>> {
     Arc::new(RwLock::new(GuiseGen {
-        guise_name: "ContactInfo".to_string(),
-        guise_id_col: "ContactId".to_string(),
+        guise_name: "users".to_string(),
+        guise_id_col: "id".to_string(),
         col_generation: Box::new(get_insert_guise_cols),
         val_generation: Box::new(get_insert_guise_vals),
     }))
