acac7748b15bc05aa8b2ab59c14bcfd37fb7e434
Malte Schwarzkopf <malte@cs.brown.edu>
2021-11-30 10:25:34 -0500

    Return private key from register_principal

--- edna-srv/src/apiproxy.rs
+++ edna-srv/src/apiproxy.rs
@@ -1,6 +1,7 @@
 use edna::EdnaClient;
 use rocket::serde::{json::Json, Deserialize};
 use rocket::State;
+use rsa::{pkcs1::ToRsaPrivateKey, RsaPrivateKey};
 use std::sync::{Arc, Mutex};
 
 #[derive(Deserialize)]
@@ -8,13 +9,23 @@ pub struct RegisterPrincipal {
     uid: edna::UID,
 }
 
+#[derive(Serialize)]
+pub struct RegisterPrincipalResponse {
+    // base64-encoded private key
+    privkey: String,
+}
+
 #[post("/register_principal", format = "json", data = "<data>")]
 pub(crate) fn register_principal(
     data: Json<RegisterPrincipal>,
     edna: &State<Arc<Mutex<EdnaClient>>>,
-) {
+) -> Json<RegisterPrincipalResponse> {
     let mut e = edna.lock().unwrap();
-    e.register_principal(data.uid.to_owned());
+    let privkey = e.register_principal(data.uid.to_owned());
+    let privkey_str = base64::encode(&privkey.to_pkcs1_der().unwrap().as_der().to_vec());
+    return Json(RegisterPrincipalResponse {
+        privkey: privkey_str,
+    });
 }
 
 #[post("/start_disguise")]
