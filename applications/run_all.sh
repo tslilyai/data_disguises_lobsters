2061f4a8923795adccf69a6275687da4866a091a
Lily Tsai <tslilyai@mit.edu>
2021-12-02 14:32:52 -0500

    fix run-all script

--- applications/run_all.sh
+++ applications/run_all.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-./websubmit-rs/server/run_benchmarks.sh
-./hotcrp/run_benchmarks.sh
-./lobsters/run_benchmarks.sh
-#./websubmit-rs/run_multiclient_benchmark.sh
+cd websubmit-rs/server
+./run_benchmarks.sh
+cd ../../hotcrp
+./run_benchmarks.sh
+cd ../lobsters
+.run_benchmarks.sh
+cd ../websubmit-rs
+./run_multiclient_benchmark.sh
