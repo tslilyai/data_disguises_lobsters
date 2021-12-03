9bfc43c04509a56a969b1bf0dd85a1147c5acb45
Lily Tsai <tslilyai@mit.edu>
2021-12-02 19:11:29 -0500

    add fig for websubmitt concurrent results

--- applications/lobsters/run_benchmarks.sh
+++ applications/lobsters/run_benchmarks.sh
@@ -7,20 +7,21 @@ mkdir output
 set -e
 
 # get stats first, prime when you do this
-for batch in '--batch' ''; do
+#for batch in '--batch' ''; do
+for batch in ''; do
     RUST_LOG=error ../../target/release/lobsters \
 		--prime $batch \
         --stats \
-		--scale 0.01 \
+		--scale 0.1 \
 		&> output/users$batch.out
 	echo "Ran stats test for users"
 done
 
-for ndisguising in 0 1; do #5 10 15 20 25 30; do
+for ndisguising in 0 1 5; do #5 10 15 20 25 30; do
     for batch in '--batch' ''; do
         RUST_LOG=error ../../target/release/lobsters \
             $batch \
-            --scale 0.01
+            --scale 0.1
             #&> output/users$batch_$ndisguising.out
         echo "Ran concurrent test for users $ndisguising disguising"
     done
