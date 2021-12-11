8b0123f6a8ad76402f6a50d2d8c05852419508ff
Lily Tsai <tslilyai@mit.edu>
2021-12-11 08:25:14 -0500

    udpate figs with newest hotcrp results

--- applications/plot_all.sh
+++ applications/plot_all.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-python3 plot_lobsters_concurrent.py
-python3 plot_websubmit_concurrent.py
 python3 plot_stats.py
 python3 plot_composition_stats.py
-python3 plot_enctest_results.py
+cp *.pdf ../papers/osdi22/figs/
+
+python3 plot_lobsters_concurrent.py
+python3 plot_websubmit_concurrent.py
+#python3 plot_enctest_results.py
 cp *.pdf ../papers/osdi22/figs/
