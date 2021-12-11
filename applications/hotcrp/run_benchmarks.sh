7cc006447eeba0510aa10ff90998a17821ac5eb4
Lily Tsai <tslilyai@mit.edu>
2021-12-11 14:26:42 -0500

    add stats for hotcrp; amp up number of users

--- applications/hotcrp/run_benchmarks.sh
+++ applications/hotcrp/run_benchmarks.sh
@@ -8,9 +8,9 @@ set -e
 
 for baseline in '--baseline' ''; do
 	RUST_LOG=warn ../../target/release/hotcrp --prime \
-		--nusers_nonpc 400 \
-		--nusers_pc 50 \
-		--npapers_rej 400 \
+		--nusers_nonpc 3000\
+		--nusers_pc 80 \
+		--npapers_rej 500 \
 		--npapers_acc 50 \
 		$baseline \
 		&> output/users_$baseline.out
