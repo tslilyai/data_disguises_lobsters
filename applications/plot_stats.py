c3b5bb317dea22c111209f08455ddaf2d51b1ce5
Lily Tsai <tslilyai@mit.edu>
2021-12-11 08:19:39 -0500

    update client op figures; add three of them now

--- applications/plot_stats.py
+++ applications/plot_stats.py
@@ -6,14 +6,14 @@ import numpy as np
 from textwrap import wrap
 
 plt.style.use('seaborn-deep')
-
-def add_labels(x,y,ax,color,offset):
+plt.figure(figsize = (6.66,3))
+def add_labels(x,y,plt,color,offset):
     for i in range(len(x)):
-        ax.text(x[i], y[i]+offset, "{0:.1f}".format(y[i]), ha='center', color=color)
+        plt.text(x[i], y[i]+offset, "{0:.1f}".format(y[i]), ha='center', color=color)
 
-def add_text_labels(x,y,ax,color,offset):
+def add_text_labels(x,y,plt,color,offset):
     for i in range(len(x)):
-        ax.text(x[i], offset, y[i], ha='center', color=color)
+        plt.text(x[i], offset, y[i], ha='center', color=color)
 
 def get_yerr(durs):
     mins = []
@@ -23,15 +23,20 @@ def get_yerr(durs):
         maxes.append(np.percentile(durs[i], 95)-statistics.median(durs[i]))
     return [mins, maxes]
 
-fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(10,12))
-axes_flat = axes.flatten()
-barwidth = 0.25
 # positions
+barwidth = 0.25
 X = np.arange(6)
-labels = ['Create Account', 'Edit\nUnanonymized\nData', 'Edit\nAnonymized\nData', 'Delete Account', 'Restore Deleted\nAccount', 'Anonymize Account']
+labels = [
+        'Create\nAccount',
+        'Edit\nUnanonymized\nData',
+        'Delete\nAccount',
+        'Anonymize\nAccount',
+        'Edit\nAnonymized\nData',
+        'Restore\nDeleted\nAccount',
+]
 
 # WEBSUBMIT RESULTS
-for (i, ax) in enumerate(axes_flat[:2]):
+for i in range(2):
     account_durs = []
     edit_durs_noanon = []
     anon_durs_batch = []
@@ -46,14 +51,14 @@ for (i, ax) in enumerate(axes_flat[:2]):
     edit_durs_baseline = []
     delete_durs_baseline = []
 
+    app = "hotcrp"
     filename_baseline = "results/hotcrp_results/hotcrp_disguise_stats_450users_baseline.csv"
     filename_batch = "results/hotcrp_results/hotcrp_disguise_stats_450users_batch.csv"
-    title = "HotCRP Reviewer Operation Latencies"
     offset = 2
     if i == 0:
+        app = "websubmit"
         filename_baseline = 'results/websubmit_results/disguise_stats_{}lec_{}users_batch_baseline.csv'.format(20, 100)
         filename_batch = 'results/websubmit_results/disguise_stats_{}lec_{}users_batch.csv'.format(20, 100)
-        title = "WebSubmit Operation Latencies"
         offset = 0.5
     with open(filename_batch,'r') as csvfile:
         rows = csvfile.readlines()
@@ -74,60 +79,61 @@ for (i, ax) in enumerate(axes_flat[:2]):
         delete_durs_baseline = [float(x)/1000 for x in rows[3].strip().split(',')]
 
     ################ add baseline closer to red line for anonymize
-    ax.bar((X-barwidth/2)[:1],
-            [statistics.median(account_durs_baseline)],
-            yerr=get_yerr([account_durs_baseline]),
-            color='g', capsize=5, width=barwidth)
-    add_labels((X-barwidth/2)[:1], [statistics.median(account_durs_baseline)], ax, 'g', offset)
-
-    ax.bar((X-barwidth/2)[1:2], [statistics.median(edit_durs_baseline)],
-            yerr=get_yerr([edit_durs_baseline]),
+    plt.bar((X-barwidth/2)[:4],
+            [
+                statistics.median(account_durs_baseline),
+                statistics.median(edit_durs_baseline),
+                statistics.median(delete_durs_baseline),
+                statistics.median(anon_durs_baseline),
+            ],
+            yerr=get_yerr([
+                account_durs_baseline,
+                edit_durs_baseline,
+                delete_durs_baseline,
+                anon_durs_baseline,
+            ]),
             color='g', capsize=5, width=barwidth, label="Manual Privacy Transformation (No Edna)")
-    add_labels((X-barwidth/2)[1:2], [statistics.median(edit_durs_baseline)], ax, 'g', offset)
-
-    ax.bar((X-barwidth/2)[3:4], [statistics.median(delete_durs_baseline)],
-            yerr=get_yerr([delete_durs_baseline]),
-            color='g', capsize=5, width=barwidth)
-    add_labels((X-barwidth/2)[3:4], [statistics.median(delete_durs_baseline)], ax, 'g', offset)
-
-    ax.bar((X-barwidth/2)[5:], [statistics.median(anon_durs_baseline)],  color='g', capsize=5, width=barwidth)
-    add_labels((X-barwidth/2)[5:], [statistics.median(anon_durs_baseline)], ax, 'g', offset)
-
-    add_text_labels((X-barwidth/2)[2:3], ["N/A"], ax, 'g', offset)
-    add_text_labels((X-barwidth/2)[4:5], ["N/A"], ax, 'g', offset)
+    add_labels((X-barwidth/2)[:4], [
+        statistics.median(account_durs_baseline),
+        statistics.median(edit_durs_baseline),
+        statistics.median(delete_durs_baseline),
+        statistics.median(anon_durs_baseline),
+    ], plt, 'g', offset)
+    add_text_labels((X-barwidth/2)[4:], ["N/A", "N/A"], plt, 'g', offset)
 
     ############### edna batched
-    ax.bar((X+barwidth/2), [
+    plt.bar((X+barwidth/2), [
         statistics.median(account_durs),
         statistics.median(edit_durs_noanon),
-        statistics.median(edit_durs_batch),
         statistics.median(delete_durs_batch_noanon),
-        statistics.median(restore_durs_batch_noanon),
         statistics.median(anon_durs_batch),
+        statistics.median(edit_durs_batch),
+        statistics.median(restore_durs_batch_noanon),
     ],
     yerr=get_yerr([
         account_durs,
         edit_durs_noanon,
-        edit_durs_batch,
         delete_durs_batch_noanon,
+        anon_durs_batch,
+        edit_durs_batch,
         restore_durs_batch_noanon,
-        anon_durs_batch
     ]),
     color='m', capsize=5, width=barwidth, label="Edna")
     add_labels((X+barwidth/2),
     [
         statistics.median(account_durs),
         statistics.median(edit_durs_noanon),
-        statistics.median(edit_durs_batch),
         statistics.median(delete_durs_batch_noanon),
-        statistics.median(restore_durs_batch_noanon),
         statistics.median(anon_durs_batch),
-    ], ax, 'm', offset)
-    ax.set_title(title)
-    ax.set_ylabel('Time (ms)')
-    ax.set_ylim(ymin=0, ymax=(np.percentile(restore_durs_batch_noanon, 95)*1.15))
-    ax.set_xticks(X)
-    ax.set_xticklabels(labels)
+        statistics.median(edit_durs_batch),
+        statistics.median(restore_durs_batch_noanon),
+    ], plt, 'm', offset)
+    plt.ylabel('Time (ms)')
+    plt.ylim(ymin=0, ymax=(np.percentile(restore_durs_batch_noanon, 95)*1.15))
+    plt.xticks(X, labels=labels)
+    plt.tight_layout(h_pad=2)
+    plt.savefig("{}_op_stats.pdf".format(app))
+    plt.clf()
 
 # LOBSTERS
 account_durs = []
@@ -169,16 +175,15 @@ with open("results/lobsters_results/lobsters_disguise_stats.csv",'r') as csvfile
         decay_durs_baseline.append(baseline_delete);
 
 X = np.arange(5)
-labels = ['Create Account',
-        'Delete Account',
-        'Decay Account',
-        'Restore Deleted\nAccount',
-        'Restore Decayed\nAccount']
-ax = axes_flat[2]
+labels = ['Create\nAccount',
+        'Delete\nAccount',
+        'Decay\nAccount',
+        'Restore\nDeleted\nAccount',
+        'Restore\nDecayed\nAccount']
 offset = 7
 
 ######################## NO EDNA
-ax.bar((X-barwidth/2)[:3], [
+plt.bar((X-barwidth/2)[:3], [
         statistics.median(account_durs_baseline),
         statistics.median(delete_durs_baseline),
         statistics.median(decay_durs_baseline)
@@ -190,12 +195,11 @@ add_labels((X-barwidth/2)[:3], [
         statistics.median(account_durs_baseline),
         statistics.median(delete_durs_baseline),
         statistics.median(decay_durs_baseline),
-    ], ax, 'g', offset)
-
-add_text_labels((X-barwidth/2)[3:], ["N/A", "N/A"], ax, 'g', offset)
+    ], plt, 'g', offset)
+add_text_labels((X-barwidth/2)[3:], ["N/A", "N/A"], plt, 'g', offset)
 
 ######################## EDNA BATCH
-ax.bar((X+barwidth/2), [
+plt.bar((X+barwidth/2), [
         statistics.median(account_durs),
         statistics.median(delete_durs_batch),
         statistics.median(decay_durs_batch),
@@ -211,16 +215,13 @@ add_labels((X+barwidth/2), [
         statistics.median(decay_durs_batch),
         statistics.median(restore_durs_batch),
         statistics.median(undecay_durs_batch)
-    ], ax, 'm', offset)
+    ], plt, 'm', offset)
 
-title = "Lobsters Operation Latencies"
-ax.set_title(title)
-ax.set_ylabel('Time (ms)')
-ax.set_ylim(ymin=0, ymax=np.percentile(restore_durs_batch,95)*1.1)
-ax.set_xticks(X)
-ax.set_xticklabels(labels)
+plt.ylabel('Time (ms)')
+plt.ylim(ymin=0, ymax=np.percentile(restore_durs_batch,95)*1.1)
+plt.xticks(X, labels=labels)
 
 # one legend per everything
-axes_flat[0].legend(loc='upper left');
-fig.tight_layout(h_pad=2)
-plt.savefig('client_op_stats.pdf', dpi=300)
+plt.legend(loc='upper left');
+plt.tight_layout(h_pad=2)
+plt.savefig('lobsters_op_stats.pdf', dpi=300)
