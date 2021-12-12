2533c40635c2b38742043fb360d94799bdbb5bf5
tslilyai <tslilyai@mit.edu>
2021-12-12 09:25:23 -0500

    nits to lobsters run

--- applications/lobsters/run_benchmarks.sh
+++ applications/lobsters/run_benchmarks.sh
@@ -6,21 +6,21 @@ set -e
 
 #get stats first, prime when you do this
 # TODO get baselines
-RUST_LOG=error perflock ../../target/release/lobsters \
-	--stats \
-	--scale 3 \
-	&> output/users.out
-echo "Ran stats primed test for users"
+#RUST_LOG=error perflock ../../target/release/lobsters \
+#	--stats \
+#	--scale 3 \
+#	&> output/users.out
+#echo "Ran stats primed test for users"
 
-#for u in 1 10 15; do
-	#for d in 'none' 'cheap' 'expensive'; do
-		#RUST_BACKTRACE=1 RUST_LOG=error perflock ../../target/release/lobsters \
-			#--scale 3 \
-			#--nsleep 0\
-			#--nconcurrent $u \
-			#--disguiser $d \
-			#--filename "${u}users_${d}" \
-		#&> output/users-$u-${d}.out
-		##echo "Ran concurrent test for $u users 0 sleep ${d}"
-	#done
-#done
+for u in 1 10 15; do
+	for d in 'none' 'cheap' 'expensive'; do
+		RUST_BACKTRACE=1 RUST_LOG=error perflock ../../target/release/lobsters \
+			--scale 3 \
+			--nsleep 0\
+			--nconcurrent $u \
+			--disguiser $d \
+			--filename "${u}users_${d}" \
+		&> output/users-$u-${d}.out
+		echo "Ran concurrent test for $u users 0 sleep ${d}"
+	done
+done
