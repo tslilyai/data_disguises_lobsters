aff3d19d9b540feac20798adebc533e135216d82
Lily Tsai <tslilyai@mit.edu>
2021-12-12 15:28:16 -0500

    clean up websubmit server impl

--- applications/websubmit-rs/server/src/email.rs
+++ applications/websubmit-rs/server/src/email.rs
@@ -1,19 +1,15 @@
-//use lettre::sendmail::SendmailTransport;
-//use lettre::Transport;
+use lettre::sendmail::SendmailTransport;
+use lettre::Transport;
 use crate::*;
 use lettre_email::Email;
-use std::fs::OpenOptions;
-use std::io::Write;
 
 pub(crate) fn send(
-    log: slog::Logger,
     sender: String,
     recipients: Vec<String>,
     subject: String,
     text: String,
 ) -> Result<(), lettre::sendmail::error::Error> {
-    //let mut mailer = SendmailTransport::new();
-
+    let mut mailer = SendmailTransport::new();
     let mut builder = Email::builder()
         .from(sender.clone())
         .subject(subject.clone())
@@ -22,44 +18,11 @@ pub(crate) fn send(
         builder = builder.to(recipient.to_string());
     }
 
-    warn!(log, "Email: Subject {}\nText: {}!", subject, text);
-
-    /*let email = builder.build();
+    let email = builder.build();
     match email {
         Ok(result) => mailer.send(result.into())?,
         Err(e) => {
             println!("couldn't construct email: {}", e);
-        }
-    }*/
-
-    // XXX for testing
-    let parts = text.split("\n");
-    for part in parts {
-        let subparts: Vec<&str> = part.split("#").collect();
-        let filename : String;
-        match subparts[0].trim() {
-            "APIKEY" => {
-                filename = format!("{}.{}", recipients[0], APIKEY_FILE);
-            }
-            "DECRYPTCAP" => {
-                filename = format!("{}.{}", recipients[0], DECRYPT_FILE);
-            }
-            "CAPS" => {
-                filename = format!("{}.{}", recipients[0], CAPS_FILE);
-            }
-            _ => {
-                warn!(log, "Badly formatted email text {}", part);
-                return Ok(())
-            }
-        };
-        let mut f = OpenOptions::new()
-            .create(true)
-            .write(true)
-            .truncate(true)
-            .open(filename)
-            .unwrap();
-        if let Err(e) = write!(f, "{}", format!("{}", subparts[1].trim())) {
-            eprintln!("Couldn't write to file: {}", e);
         }
     }
     Ok(())
