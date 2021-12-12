6a7861d751435cecc5995d38bd01527a3d695020
Lily Tsai <tslilyai@mit.edu>
2021-12-12 15:09:28 -0500

    split composition stats into two

--- applications/plot_composition_stats.py
+++ applications/plot_composition_stats.py
@@ -18,13 +18,13 @@ matplotlib.rc('figure', figsize=(3.33,1.5))
 matplotlib.rc('axes', linewidth=0.5)
 matplotlib.rc('lines', linewidth=0.5)
 
-def add_labels(x,y,ax,color,offset):
+def add_labels(x,y,plt,color,offset):
     for i in range(len(x)):
-        ax.text(x[i], y[i]+offset, "{0:.1f}".format(y[i]), ha='center', color=color)
+        plt.text(x[i], y[i]+offset, "{0:.1f}".format(y[i]), ha='center', color=color)
 
-def add_text_labels(x,y,ax,color,offset):
+def add_text_labels(x,y,plt,color,offset):
     for i in range(len(x)):
-        ax.text(x[i], offset, y[i], ha='center', color=color)
+        plt.text(x[i], offset, y[i], ha='center', color=color)
 
 def get_yerr(durs):
     mins = []
@@ -34,15 +34,13 @@ def get_yerr(durs):
         maxes.append(np.percentile(durs[i], 95)-statistics.median(durs[i]))
     return [mins, maxes]
 
-fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(3.33,3))
-axes_flat = axes.flatten()
 barwidth = 0.25
 # positions
 X = np.arange(2)
 labels = ['Delete Account', 'Restore Deleted\nAccount']
 
 # WEBSUBMIT/HOTCRP RESULTS
-for (i, ax) in enumerate(axes_flat[:2]):
+for i in range(2):
     delete_durs_baseline = []
     delete_durs_batch = []
     restore_durs_batch = []
@@ -51,12 +49,12 @@ for (i, ax) in enumerate(axes_flat[:2]):
 
     filename_baseline = "results/hotcrp_results/hotcrp_disguise_stats_3080users_baseline.csv"
     filename_batch = "results/hotcrp_results/hotcrp_disguise_stats_3080users_batch.csv"
-    title = "HotCRP Reviewer Disguise Costs w/Composition"
+    title = "hotcrp"
     offset = 50
     if i == 0:
         filename_baseline = 'results/websubmit_results/disguise_stats_{}lec_{}users_baseline.csv'.format(20, 100)
         filename_batch = 'results/websubmit_results/disguise_stats_{}lec_{}users.csv'.format(20, 100)
-        title = "WebSubmit Disguise Costs w/Composition"
+        title = "websubmit"
         offset = 10
     with open(filename_batch,'r') as csvfile:
         rows = csvfile.readlines()
@@ -70,14 +68,14 @@ for (i, ax) in enumerate(axes_flat[:2]):
         delete_durs_baseline = [float(x)/1000 for x in rows[3].strip().split(',')]
 
     ################ add baseline closer to red line for anonymize
-    ax.bar((X-barwidth)[:1], [statistics.median(delete_durs_baseline)],
+    plt.bar((X-barwidth)[:1], [statistics.median(delete_durs_baseline)],
             yerr=get_yerr([delete_durs_baseline]),
             color='g', capsize=5, width=barwidth)
-    add_labels((X-barwidth)[:1], [statistics.median(delete_durs_baseline)], ax, 'g', offset)
-    add_text_labels((X-barwidth)[1:], ["N/A"], ax, 'g', offset)
+    add_labels((X-barwidth)[:1], [statistics.median(delete_durs_baseline)], plt, 'g', offset)
+    add_text_labels((X-barwidth)[1:], ["N/A"], plt, 'g', offset)
 
     ############### edna w/out composition
-    ax.bar((X), [
+    plt.bar((X), [
         statistics.median(delete_durs_batch_noanon),
         statistics.median(restore_durs_batch_noanon),
     ],
@@ -90,10 +88,10 @@ for (i, ax) in enumerate(axes_flat[:2]):
     [
         statistics.median(delete_durs_batch_noanon),
         statistics.median(restore_durs_batch_noanon),
-    ], ax, 'm', offset)
+    ], plt, 'm', offset)
 
     ############### edna w/composition
-    ax.bar((X+barwidth), [
+    plt.bar((X+barwidth), [
         statistics.median(delete_durs_batch),
         statistics.median(restore_durs_batch),
     ],
@@ -106,15 +104,12 @@ for (i, ax) in enumerate(axes_flat[:2]):
     [
         statistics.median(delete_durs_batch),
         statistics.median(restore_durs_batch),
-    ], ax, 'c', offset)
+    ], plt, 'c', offset)
 
-    ax.set_title(title)
-    ax.set_ylabel('Time (ms)')
-    ax.set_ylim(ymin=0, ymax=(np.percentile(restore_durs_batch, 95)*1.15))
-    ax.set_xticks(X)
-    ax.set_xticklabels(labels)
-
-# one legend per everything
-axes_flat[0].legend(loc='upper left');
-plt.tight_layout(h_pad=1)
-plt.savefig('composition_stats.pdf', dpi=300)
+    plt.ylabel('Time (ms)')
+    plt.ylim(ymin=0, ymax=(np.percentile(restore_durs_batch, 95)*1.15))
+    plt.xticks(X, labels=labels)
+    plt.legend(loc='upper left');
+    plt.tight_layout(h_pad=1)
+    plt.savefig('composition_stats_{}.pdf'.format(title), dpi=300)
+    plt.clf()
